79daa659135f5c1f5fe73e401e0322c754389756
Germain <germain@souquet.com>
2023-03-07 15:55:52 +0000

    Add browser support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import browserslistToEsbuild from "browserslist-to-esbuild";
 
 export default defineConfig({
   publicDir: "res",
   build: {
+    target: browserslistToEsbuild(),
     assetsInlineLimit: 0, // To disable assets inlining as base64 in the bundle
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
