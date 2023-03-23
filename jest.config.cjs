7b35d71b891192eb85558dd2b40ce87c5ee31193
Germain <germain@souquet.com>
2023-03-23 09:44:39 +0100

    remove unneeded diff

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,6 +4,5 @@ module.exports = {
   "setupFilesAfterEnv": ['<rootDir>/src/setupTests.ts'],
   "moduleNameMapper": {
     "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
-    "\\.svg$": "<rootDir>/node_modules/jest-css-modules",
   }
 }
