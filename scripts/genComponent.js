4974c452d3cdc34bbfa802bcc1155825d153a405
Germain <germain@souquet.com>
2023-03-24 10:35:18 +0100

    Fix eslint warning and add files to ignore

--- scripts/genComponent.js
+++ scripts/genComponent.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { argv, argv0 } from "process";
+import { argv } from "process";
 import fs from "fs";
 import path from "path";
 
