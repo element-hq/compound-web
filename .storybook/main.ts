e6035d84d69ec17ca864351b0f754def166728e4
Germain <germain@souquet.com>
2023-04-27 10:23:14 +0100

    Set correct storybook canvas bg color (#21)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-designs",
+    "storybook-addon-themes",
   ],
   framework: {
     name: "@storybook/react-vite",
