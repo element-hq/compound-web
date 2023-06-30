fac5f22ef273a89393245f00bc9d226c136935f9
germain-gg <germain-gg@users.noreply.github.com>
2023-06-30 10:03:24 +0000

    deploy: 39aff6e8410cffca7667ff8515604f044310bd4c

---
+++ src/components/Avatar/useIdColorHash.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Determines a number for a given Matrix ID or room ID, helps disambiguating users
+ * who are trying to impersonate someone else.
+ * @param id - a Matrix ID or room ID
+ * @returns a hash of the ID provided
+ */
+export declare function useIdColorHash(id: string): number;
+//# sourceMappingURL=useIdColorHash.d.ts.map
--- src/components/Avatar/useUsernameColor.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Determines a color to assign to a given username
- * @param id - a Matrix username ID
- * @returns a hexadecimal color
- */
-export declare function useUsernameColor(id: string): number;
-//# sourceMappingURL=useUsernameColor.d.ts.map
