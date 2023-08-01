4921f1e1385468c4940698a2f00092e35d4509ee
Quentin Gliech <quentingliech@gmail.com>
2023-08-01 16:36:25 +0200

    Remove Jest config

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-  moduleNameMapper: {
-    "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
-    "\\.(svg)$": "<rootDir>/scripts/svgr.js",
-  },
-};
