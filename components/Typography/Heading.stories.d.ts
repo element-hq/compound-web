14825be600d9b04d2c8af4d71431d8ab8c381d74
germain-gg <germain-gg@users.noreply.github.com>
2023-08-09 14:23:33 +0000

    deploy: 71af95cc8aa352f82d9b1570f40343786616535a

--- components/Typography/Heading.stories.d.ts
+++ components/Typography/Heading.stories.d.ts
@@ -2,7 +2,7 @@ import React from "react";
 declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react/dist/types-0a347bb9").R, Omit<{
     as?: React.ElementType<any> | undefined;
     type?: "body" | "heading" | undefined;
-    weight?: "bold" | "regular" | "semibold" | "medium" | undefined;
+    weight?: "bold" | "medium" | "regular" | "semibold" | undefined;
     size?: "sm" | "lg" | "xs" | "md" | undefined;
     className?: string | undefined;
 } & Omit<any, "ref"> & {
@@ -12,7 +12,7 @@ export default _default;
 export declare const Heading: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, Omit<{
     as?: React.ElementType<any> | undefined;
     type?: "body" | "heading" | undefined;
-    weight?: "bold" | "regular" | "semibold" | "medium" | undefined;
+    weight?: "bold" | "medium" | "regular" | "semibold" | undefined;
     size?: "sm" | "lg" | "xs" | "md" | undefined;
     className?: string | undefined;
 } & Omit<any, "ref"> & {
