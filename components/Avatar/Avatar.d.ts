cc51a750fe6e227b88f5d515d92412f25282ff19
germain-gg <germain-gg@users.noreply.github.com>
2023-08-10 10:57:53 +0000

    deploy: 0b231c1c37b19507ec7db0f70a7bae0a0c4c6770

--- components/Avatar/Avatar.d.ts
+++ components/Avatar/Avatar.d.ts
@@ -1,12 +1,85 @@
 import React from "react";
 import { SuspenseImg } from "../../utils/SuspenseImg";
-type AvatarProps = JSX.IntrinsicElements["span"] & {
-    src?: React.ComponentProps<typeof SuspenseImg>["src"];
+/**
+ * Avatar component that will fallback to an initial letter over a coloured
+ * background if no source is provided or if the source has failed to load.
+ */
+export declare const Avatar: React.ForwardRefExoticComponent<(Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+    /**
+     * The avatar image URL, if any.
+     */
+    src?: string | undefined;
+    /**
+     * The Matrix ID, Room ID, or Alias to generate the color when no image source
+     * is provided. Also used as a fallback when name is empty.
+     */
     id: string;
+    /**
+     * The name used for the initial letter displayed when no image source is provided.
+     */
     name: string;
-    type?: "square" | "round";
-    size?: CSSStyleDeclaration["height"];
+    /**
+     * Defines the avatar type, typically round, square is usually for spaces.
+     * @default "round"
+     */
+    type?: "round" | "square" | undefined;
+    /**
+     * The avatar size in CSS units, e.g. `"24px"`.
+     */
+    size?: string | undefined;
+    /**
+     * On click handler, will turn the avatar into a button element.
+     */
+    onClick?: ((e: React.MouseEvent) => void) | undefined;
+    /**
+     * Key down handler, will turn the avatar into a button element.
+     */
+    onKeyDown?: ((e: React.KeyboardEvent) => void) | undefined;
+    /**
+     * Key up handler, will turn the avatar into a button element.
+     */
+    onKeyUp?: ((e: React.KeyboardEvent) => void) | undefined;
+    /**
+     * Callback when the image has failed to load.
+     */
     onError?: React.ComponentProps<typeof SuspenseImg>["onError"];
-};
-export declare const Avatar: React.ForwardRefExoticComponent<Omit<AvatarProps, "ref"> & React.RefAttributes<HTMLSpanElement>>;
-export {};
+}, "ref"> | Omit<React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    /**
+     * The avatar image URL, if any.
+     */
+    src?: string | undefined;
+    /**
+     * The Matrix ID, Room ID, or Alias to generate the color when no image source
+     * is provided. Also used as a fallback when name is empty.
+     */
+    id: string;
+    /**
+     * The name used for the initial letter displayed when no image source is provided.
+     */
+    name: string;
+    /**
+     * Defines the avatar type, typically round, square is usually for spaces.
+     * @default "round"
+     */
+    type?: "round" | "square" | undefined;
+    /**
+     * The avatar size in CSS units, e.g. `"24px"`.
+     */
+    size?: string | undefined;
+    /**
+     * On click handler, will turn the avatar into a button element.
+     */
+    onClick?: ((e: React.MouseEvent) => void) | undefined;
+    /**
+     * Key down handler, will turn the avatar into a button element.
+     */
+    onKeyDown?: ((e: React.KeyboardEvent) => void) | undefined;
+    /**
+     * Key up handler, will turn the avatar into a button element.
+     */
+    onKeyUp?: ((e: React.KeyboardEvent) => void) | undefined;
+    /**
+     * Callback when the image has failed to load.
+     */
+    onError?: React.ComponentProps<typeof SuspenseImg>["onError"];
+}, "ref">) & React.RefAttributes<HTMLSpanElement | HTMLButtonElement>>;
--- components/Avatar/Avatar.stories.d.ts
+++ components/Avatar/Avatar.stories.d.ts
@@ -1,42 +1,149 @@
 import React from "react";
-declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react/dist/types-0a347bb9").R, Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react/dist/types-0a347bb9").R, (Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
     src?: string | undefined;
     id: string;
     name: string;
     type?: "round" | "square" | undefined;
     size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
     onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
-}, "ref"> & React.RefAttributes<HTMLSpanElement>>;
+}, "ref"> | Omit<React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    src?: string | undefined;
+    id: string;
+    name: string;
+    type?: "round" | "square" | undefined;
+    size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
+}, "ref">) & React.RefAttributes<HTMLSpanElement | HTMLButtonElement>>;
 export default _default;
-export declare const Round: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+export declare const Round: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, (Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
     src?: string | undefined;
     id: string;
     name: string;
     type?: "round" | "square" | undefined;
     size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
     onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
-}, "ref"> & React.RefAttributes<HTMLSpanElement>>;
-export declare const Square: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+}, "ref"> | Omit<React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement> & {
     src?: string | undefined;
     id: string;
     name: string;
     type?: "round" | "square" | undefined;
     size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
     onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
-}, "ref"> & React.RefAttributes<HTMLSpanElement>>;
-export declare const NoImageFallback: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+}, "ref">) & React.RefAttributes<HTMLSpanElement | HTMLButtonElement>>;
+export declare const Square: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, (Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
     src?: string | undefined;
     id: string;
     name: string;
     type?: "round" | "square" | undefined;
     size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
     onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
-}, "ref"> & React.RefAttributes<HTMLSpanElement>>;
-export declare const LargeNoImageFallback: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+}, "ref"> | Omit<React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement> & {
     src?: string | undefined;
     id: string;
     name: string;
     type?: "round" | "square" | undefined;
     size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
     onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
-}, "ref"> & React.RefAttributes<HTMLSpanElement>>;
+}, "ref">) & React.RefAttributes<HTMLSpanElement | HTMLButtonElement>>;
+export declare const Button: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, (Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+    src?: string | undefined;
+    id: string;
+    name: string;
+    type?: "round" | "square" | undefined;
+    size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
+}, "ref"> | Omit<React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    src?: string | undefined;
+    id: string;
+    name: string;
+    type?: "round" | "square" | undefined;
+    size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
+}, "ref">) & React.RefAttributes<HTMLSpanElement | HTMLButtonElement>>;
+export declare const NoImageFallback: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, (Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+    src?: string | undefined;
+    id: string;
+    name: string;
+    type?: "round" | "square" | undefined;
+    size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
+}, "ref"> | Omit<React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    src?: string | undefined;
+    id: string;
+    name: string;
+    type?: "round" | "square" | undefined;
+    size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
+}, "ref">) & React.RefAttributes<HTMLSpanElement | HTMLButtonElement>>;
+export declare const LargeNoImageFallback: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, (Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+    src?: string | undefined;
+    id: string;
+    name: string;
+    type?: "round" | "square" | undefined;
+    size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
+}, "ref"> | Omit<React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    src?: string | undefined;
+    id: string;
+    name: string;
+    type?: "round" | "square" | undefined;
+    size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
+}, "ref">) & React.RefAttributes<HTMLSpanElement | HTMLButtonElement>>;
+export declare const AllAvatars: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, (Omit<React.ClassAttributes<HTMLSpanElement> & React.HTMLAttributes<HTMLSpanElement> & {
+    src?: string | undefined;
+    id: string;
+    name: string;
+    type?: "round" | "square" | undefined;
+    size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
+}, "ref"> | Omit<React.ClassAttributes<HTMLButtonElement> & React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    src?: string | undefined;
+    id: string;
+    name: string;
+    type?: "round" | "square" | undefined;
+    size?: string | undefined;
+    onClick?: ((e: React.MouseEvent<Element, MouseEvent>) => void) | undefined;
+    onKeyDown?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onKeyUp?: ((e: React.KeyboardEvent<Element>) => void) | undefined;
+    onError?: React.ReactEventHandler<HTMLImageElement> | undefined;
+}, "ref">) & React.RefAttributes<HTMLSpanElement | HTMLButtonElement>>;
