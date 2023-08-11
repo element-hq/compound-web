adc77788348e95325bbdb0611b48dfa52df2a222
Germain <germains@element.io>
2023-08-11 09:48:51 +0100

    Add JSDoc comments for ActionControl component

--- src/components/ActionControl/ActionControl.tsx
+++ src/components/ActionControl/ActionControl.tsx
@@ -21,13 +21,30 @@ import styles from "./ActionControl.module.css";
 import { Control, Field, Root } from "../Form";
 
 type ActionControlProps = {
+  /**
+   * The CSS class name.
+   */
   className?: string;
+  /**
+   * The React component to display on the right hand side of the icon.
+   */
   Icon: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  /**
+   * On click callback for the icon on the right hand side
+   */
   onActionClick: (e: React.MouseEvent) => void;
+  /**
+   * The name of the action to perform (e.g. "Copy to clipboard")
+   */
   actionLabel: string;
   disabled?: boolean;
 } & React.ComponentProps<typeof Control>;
 
+/**
+ * A generic input with an action icon on the right hand side.
+ * You should rarely use it directly,
+ * but it is powering the likes of "copy to clipboard input", "password reveal", ...
+ */
 export const ActionControl = forwardRef<
   HTMLInputElement,
   PropsWithChildren<ActionControlProps>
