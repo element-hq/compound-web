b392a1c479ca77895160bb2c9ee5ca9593b65743
Germain <germains@element.io>
2023-08-16 08:03:40 +0100

    Create IconButton component

---
+++ src/components/IconButton/IconButton.stories.tsx
@@ -0,0 +1,48 @@
+/*
+Copyright 2023 New Vector Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+
+import { IconButton as IconButtonComponent } from "./IconButton";
+
+import UserIcon from "@vector-im/compound-design-tokens/icons/user.svg";
+
+export default {
+  title: "IconButton",
+  component: IconButtonComponent,
+  argTypes: {},
+  args: {},
+} as Meta<typeof IconButtonComponent>;
+
+const Template: StoryFn<typeof IconButtonComponent> = (args) => (
+  <IconButtonComponent {...args}>
+    <UserIcon />
+  </IconButtonComponent>
+);
+
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const WithIndicator = Template.bind({});
+WithIndicator.args = {
+  indicator: "default",
+};
+
+export const WithCriticalIndicator = Template.bind({});
+WithCriticalIndicator.args = {
+  indicator: "critical",
+};
