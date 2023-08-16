a32851ad552a0a83d6659a88d043663becd63f46
germain-gg <germain-gg@users.noreply.github.com>
2023-08-16 06:27:15 +0000

    deploy: 826cb9a861c8fd731491e197593da777546d6235

---
+++ assets/preview-f24db7b0.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-aa173408.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-b055a3bc.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-b055a3bc.js","./iframe-aa173408.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-725791a0.js","./index-d3ea75b5.js","./index-d475d2ea.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-f24db7b0.js.map
--- utils/SuspenseImg.d.ts
+++ utils/SuspenseImg.d.ts
@@ -1,6 +1,33 @@
 import React from "react";
+interface ImageLoadingCache {
+    /**
+     * A map of all sources loaded with this cache instance
+     * Contains a promise if the image is being loaded, and `true` if the source
+     * has succesfully been loaded.
+     */
+    __cache: Map<string, Promise<unknown> | boolean>;
+    /**
+     * Will attempt to load the source and will throw an exception until
+     * the image has been loaded succesfully.
+     * The exception will be caught by React Suspense and that will notify it to
+     * display the fallback
+     * @param src the image source.
+     * @returns true if the source has been loaded successfully
+     */
+    read: (src: string) => boolean;
+}
 type SuspenseImgProps = JSX.IntrinsicElements["img"] & {
+    /**
+     * The source of the image to load
+     */
     src: string;
+    /**
+     * The cache instance to drive the suspense loading
+     * Useful to override in a test environment
+     * @default imgCache a generic cache instance shared globally
+     */
+    cache?: ImageLoadingCache;
 };
 export declare const SuspenseImg: React.FC<SuspenseImgProps>;
 export {};
+//# sourceMappingURL=SuspenseImg.d.ts.map
