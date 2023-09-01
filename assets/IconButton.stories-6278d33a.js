aeb8a5f36e20b25731dec45b73cf1ea03783eb28
germain-gg <germain-gg@users.noreply.github.com>
2023-09-01 06:21:10 +0000

    deploy: 0682d6afd9a9d5e1855a96807fcda5b0a15befa8

--- assets/IconButton.stories-6278d33a.js
+++ assets/IconButton.stories-6278d33a.js
@@ -1,4 +1,4 @@
-import{j as n}from"./jsx-runtime-ffb262ed.js";import{I as r,S as c}from"./user-bee931e4.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-e131923d.js";const j={title:"IconButton",component:r,argTypes:{},args:{}},a=s=>n.jsxs(n.Fragment,{children:[n.jsx(r,{...s,size:"32px",children:n.jsx(c,{})}),n.jsx(r,{...s,size:"48px",children:n.jsx(c,{})}),n.jsx(r,{...s,size:"64px",children:n.jsx(c,{})})]}),o=a.bind({});o.args={};const t=a.bind({});t.args={indicator:"default"};const e=a.bind({});e.args={indicator:"highlight"};var p,i,m;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`args => <>
+import{j as n}from"./jsx-runtime-ffb262ed.js";import{I as r,S as c}from"./user-90b0e66c.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-e131923d.js";const j={title:"IconButton",component:r,argTypes:{},args:{}},a=s=>n.jsxs(n.Fragment,{children:[n.jsx(r,{...s,size:"32px",children:n.jsx(c,{})}),n.jsx(r,{...s,size:"48px",children:n.jsx(c,{})}),n.jsx(r,{...s,size:"64px",children:n.jsx(c,{})})]}),o=a.bind({});o.args={};const t=a.bind({});t.args={indicator:"default"};const e=a.bind({});e.args={indicator:"highlight"};var p,i,m;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`args => <>
     <IconButtonComponent {...args} size="32px">
       <UserIcon />
     </IconButtonComponent>
@@ -35,4 +35,4 @@ import{j as n}from"./jsx-runtime-ffb262ed.js";import{I as r,S as c}from"./user-b
       <UserIcon />
     </IconButtonComponent>
   </>`,...(B=(x=e.parameters)==null?void 0:x.docs)==null?void 0:B.source}}};const S=["Normal","WithIndicator","WithHighlightIndicator"];export{o as Normal,e as WithHighlightIndicator,t as WithIndicator,S as __namedExportsOrder,j as default};
-//# sourceMappingURL=IconButton.stories-6eda9b0a.js.map
+//# sourceMappingURL=IconButton.stories-6278d33a.js.map
