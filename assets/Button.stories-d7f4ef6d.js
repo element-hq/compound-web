126cab202be35c5866fcd98f95c710236e1e6d6a
sandhose <sandhose@users.noreply.github.com>
2023-09-08 07:23:22 +0000

    deploy: 3fa621674055c7582b6631d34fe6b0b8cc4923d0

---
+++ assets/Button.stories-d7f4ef6d.js
@@ -0,0 +1,34 @@
+import{j as i}from"./jsx-runtime-ffb262ed.js";import{B as s}from"./Button-86cf2062.js";import{S as x}from"./visibility-on-2a2c44f8.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-e131923d.js";const I={title:"Button",component:s,argTypes:{size:{options:["sm","lg"],control:{type:"inline-radio"}}},args:{}},r=a=>i.jsxs("div",{style:{display:"flex",gap:8},children:[i.jsx(s,{...a,children:"Click me!"}),i.jsx(s,{Icon:x,...a,children:"Click me!"})]}),t=r.bind({});t.args={kind:"primary"};t.parameters={design:{type:"figma",url:"https://www.figma.com/file/HUysJAhv6cK6p1Pc81Fxaa/Matrix-Authentication-Service-(MAS)?node-id=101%3A16968&t=gc1qRwZLEob0m7OM-1"}};const n=r.bind({});n.args={kind:"secondary"};n.parameters={design:{type:"figma",url:"https://www.figma.com/file/HUysJAhv6cK6p1Pc81Fxaa/Matrix-Authentication-Service-(MAS)?node-id=101%3A16968&t=gc1qRwZLEob0m7OM-1"}};const o=r.bind({});o.args={kind:"tertiary"};const e=r.bind({});e.args={kind:"destructive"};e.parameters={design:{type:"figma",url:"https://www.figma.com/file/HUysJAhv6cK6p1Pc81Fxaa/Matrix-Authentication-Service-(MAS)?node-id=101%3A16968&t=gc1qRwZLEob0m7OM-1"}};var c,m,p;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`args => <div style={{
+  display: "flex",
+  gap: 8
+}}>
+    <ButtonComponent {...args}>Click me!</ButtonComponent>
+    <ButtonComponent Icon={VisibilityOnIcon} {...args}>
+      Click me!
+    </ButtonComponent>
+  </div>`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var d,l,u;n.parameters={...n.parameters,docs:{...(d=n.parameters)==null?void 0:d.docs,source:{originalSource:`args => <div style={{
+  display: "flex",
+  gap: 8
+}}>
+    <ButtonComponent {...args}>Click me!</ButtonComponent>
+    <ButtonComponent Icon={VisibilityOnIcon} {...args}>
+      Click me!
+    </ButtonComponent>
+  </div>`,...(u=(l=n.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var g,y,C;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`args => <div style={{
+  display: "flex",
+  gap: 8
+}}>
+    <ButtonComponent {...args}>Click me!</ButtonComponent>
+    <ButtonComponent Icon={VisibilityOnIcon} {...args}>
+      Click me!
+    </ButtonComponent>
+  </div>`,...(C=(y=o.parameters)==null?void 0:y.docs)==null?void 0:C.source}}};var B,v,f;e.parameters={...e.parameters,docs:{...(B=e.parameters)==null?void 0:B.docs,source:{originalSource:`args => <div style={{
+  display: "flex",
+  gap: 8
+}}>
+    <ButtonComponent {...args}>Click me!</ButtonComponent>
+    <ButtonComponent Icon={VisibilityOnIcon} {...args}>
+      Click me!
+    </ButtonComponent>
+  </div>`,...(f=(v=e.parameters)==null?void 0:v.docs)==null?void 0:f.source}}};const M=["Primary","Secondary","Tertiary","Destructive"];export{e as Destructive,t as Primary,n as Secondary,o as Tertiary,M as __namedExportsOrder,I as default};
+//# sourceMappingURL=Button.stories-d7f4ef6d.js.map
--- assets/preview-619dcafe.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-f9876fb9.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-746a7ff9.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-746a7ff9.js","./iframe-f9876fb9.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-725791a0.js","./index-d3ea75b5.js","./index-d475d2ea.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-619dcafe.js.map
