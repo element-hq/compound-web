7aa54869f2f582360d0f7cb38a48577707d2510e
sandhose <sandhose@users.noreply.github.com>
2023-09-18 06:33:08 +0000

    deploy: e7f673d885282555e47380943f7162746058dc07

--- components/Button/Button.d.ts
+++ components/Button/Button.d.ts
@@ -1,10 +1,11 @@
 import React, { ComponentType, PropsWithChildren } from "react";
-type ButtonProps<C extends React.ElementType> = {
-    /**
-     * The underlying HTML element to use. Can be a button or a link.
-     * @default "button"
-     */
-    as?: C;
+interface ButtonComponent {
+    <C extends React.ElementType>(props: {
+        as: C;
+    } & ButtonPropsFor<C>): React.ReactElement;
+    (props: ButtonPropsFor<"button">): React.ReactElement;
+}
+type ButtonOwnProps = PropsWithChildren<{
     /**
      * The type of button.
      */
@@ -14,17 +15,16 @@ type ButtonProps<C extends React.ElementType> = {
      */
     size?: "sm" | "lg";
     /**
-     * The CSS class name.
-     */
-    className?: string;
-    /**
      * An icon to display within the button.
      */
     Icon?: ComponentType<React.SVGAttributes<SVGElement>>;
-} & React.ComponentPropsWithoutRef<C>;
+}>;
+type ButtonPropsFor<C extends React.ElementType> = ButtonOwnProps & Omit<React.ComponentPropsWithoutRef<C>, keyof ButtonOwnProps | "as"> & {
+    ref?: React.Ref<C>;
+};
 /**
  * A button component that can be transformed into a link, but keep the button
  * styling using the `as` property.
  */
-export declare const Button: <C extends React.ElementType<any> = "button">({ as, kind, size, children, className, Icon, ...props }: React.PropsWithChildren<ButtonProps<C>>) => React.ReactElement;
+export declare const Button: ButtonComponent;
 export {};
