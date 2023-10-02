cb21c1fbb50d466b12c0d5390fa2f5f691f7b996
Robin <robin@robin.town>
2023-10-02 12:19:43 -0400

    Make the testing snapshot a bit smaller

--- src/components/FloatingMenu/FloatingMenu.test.tsx
+++ src/components/FloatingMenu/FloatingMenu.test.tsx
@@ -18,21 +18,17 @@ import { describe, it, expect } from "vitest";
 import { render } from "@testing-library/react";
 import React from "react";
 import UserProfileIcon from "@vector-im/compound-design-tokens/icons/user-profile.svg";
-import NotificationsIcon from "@vector-im/compound-design-tokens/icons/notifications.svg";
-import ChatProblemIcon from "@vector-im/compound-design-tokens/icons/chat-problem.svg";
 import LeaveIcon from "@vector-im/compound-design-tokens/icons/leave.svg";
 
 import { FloatingMenu } from "./FloatingMenu";
 import { MenuItem } from "../MenuItem/MenuItem";
 import { MenuDivider } from "../MenuItem/MenuDivider";
 
-describe("Menu", () => {
+describe("FloatingMenu", () => {
   it("renders", () => {
     const { asFragment } = render(
       <FloatingMenu size={200} title="Settings">
         <MenuItem Icon={UserProfileIcon} label="Profile" />
-        <MenuItem Icon={NotificationsIcon} label="Notifications" />
-        <MenuItem Icon={ChatProblemIcon} label="Feedback" />
         <MenuDivider />
         <MenuItem kind="critical" Icon={LeaveIcon} label="Sign out" />
       </FloatingMenu>,
