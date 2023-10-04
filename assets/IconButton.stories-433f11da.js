7d54776173ca451663477bca00a34c975649e07c
germain-gg <germain-gg@users.noreply.github.com>
2023-10-04 09:35:23 +0000

    deploy: 3c5446854afcff8ddc70704ac02990330aa9dc2b

---
+++ assets/IconButton.stories-433f11da.js
@@ -0,0 +1,38 @@
+import{j as n}from"./jsx-runtime-ffb262ed.js";import{I as r,S as c}from"./user-profile-74ab4b5a.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-e131923d.js";const j={title:"IconButton",component:r,argTypes:{},args:{}},a=s=>n.jsxs(n.Fragment,{children:[n.jsx(r,{...s,size:"32px",children:n.jsx(c,{})}),n.jsx(r,{...s,size:"48px",children:n.jsx(c,{})}),n.jsx(r,{...s,size:"64px",children:n.jsx(c,{})})]}),o=a.bind({});o.args={};const t=a.bind({});t.args={indicator:"default"};const e=a.bind({});e.args={indicator:"highlight"};var p,i,m;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`args => <>
+    <IconButtonComponent {...args} size="32px">
+      <UserIcon />
+    </IconButtonComponent>
+
+    <IconButtonComponent {...args} size="48px">
+      <UserIcon />
+    </IconButtonComponent>
+
+    <IconButtonComponent {...args} size="64px">
+      <UserIcon />
+    </IconButtonComponent>
+  </>`,...(m=(i=o.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var I,u,d;t.parameters={...t.parameters,docs:{...(I=t.parameters)==null?void 0:I.docs,source:{originalSource:`args => <>
+    <IconButtonComponent {...args} size="32px">
+      <UserIcon />
+    </IconButtonComponent>
+
+    <IconButtonComponent {...args} size="48px">
+      <UserIcon />
+    </IconButtonComponent>
+
+    <IconButtonComponent {...args} size="64px">
+      <UserIcon />
+    </IconButtonComponent>
+  </>`,...(d=(u=t.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var g,x,B;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`args => <>
+    <IconButtonComponent {...args} size="32px">
+      <UserIcon />
+    </IconButtonComponent>
+
+    <IconButtonComponent {...args} size="48px">
+      <UserIcon />
+    </IconButtonComponent>
+
+    <IconButtonComponent {...args} size="64px">
+      <UserIcon />
+    </IconButtonComponent>
+  </>`,...(B=(x=e.parameters)==null?void 0:x.docs)==null?void 0:B.source}}};const f=["Normal","WithIndicator","WithHighlightIndicator"];export{o as Normal,e as WithHighlightIndicator,t as WithIndicator,f as __namedExportsOrder,j as default};
+//# sourceMappingURL=IconButton.stories-433f11da.js.map
---
+++ assets/preview-5f8087e9.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-f693ec36.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-92e17146.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-92e17146.js","./iframe-f693ec36.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-725791a0.js","./index-d3ea75b5.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-5f8087e9.js.map
