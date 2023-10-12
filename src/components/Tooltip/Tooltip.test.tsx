2f7f7c45b66e448172e46474c9e48fd83732f4b5
Germain <germain@souquet.com>
2023-10-12 09:06:37 +0100

    Tooltip: rename shortcut to caption and styling fixes (#103)

--- src/components/Tooltip/Tooltip.test.tsx
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -27,7 +27,7 @@ describe("Tooltip", () => {
   });
   it("renders open by default", () => {
     const { asFragment } = render(
-      <Tooltip label="Hello world 👋" shortcut="⌘ + C" open={true}>
+      <Tooltip label="Hello world 👋" caption="⌘ + C" open={true}>
         <IconButton>
           <svg />
         </IconButton>
