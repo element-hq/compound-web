66f755c7c87fe52f0b8b36607673814cf2b00777
Quentin Gliech <quentingliech@gmail.com>
2023-10-13 14:48:37 +0200

    Revamp MFA controls

---
+++ src/components/Form/Controls/MFA/MFA.stories.tsx
@@ -0,0 +1,154 @@
+/*
+Copyright 2023 The Matrix.org Foundation C.I.C.
+Copyright 2023 New Vector Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React from "react";
+
+import { Meta, StoryObj } from "@storybook/react";
+import { within } from "@storybook/testing-library";
+
+import * as Form from "../../";
+import { MFAControl } from "./MFA";
+
+type Props = React.ComponentProps<typeof MFAControl> & {
+  label?: string;
+  invalid?: boolean;
+  message?: string;
+};
+
+export default {
+  title: "MFA",
+  component: MFAControl,
+  render: ({ label, invalid, message, ...props }) => (
+    <Form.Root>
+      <Form.Field name="story" serverInvalid={invalid}>
+        {label && <Form.Label>{label}</Form.Label>}
+        <MFAControl {...props} />
+        {message && (
+          <Form.Message kind={invalid ? "error" : "help"}>
+            {message}
+          </Form.Message>
+        )}
+      </Form.Field>
+    </Form.Root>
+  ),
+  parameters: {
+    controls: {
+      include: ["value", "disabled", "readOnly", "label", "invalid", "message"],
+    },
+  },
+  argTypes: {
+    value: {
+      type: "string",
+    },
+    disabled: {
+      type: "boolean",
+    },
+    readOnly: {
+      type: "boolean",
+    },
+    label: {
+      type: "string",
+    },
+    invalid: {
+      type: "boolean",
+    },
+    message: {
+      type: "string",
+    },
+  },
+  args: {
+    disabled: false,
+    readOnly: false,
+    invalid: false,
+    message: "Help text.",
+  },
+} satisfies Meta<Props>;
+
+type Story = StoryObj<Props>;
+
+export const Basic: Story = {
+  args: {
+    label: "Basic",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    (canvas.getByRole("textbox") as HTMLInputElement).value = "0123";
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: "Disabled",
+    value: "012345",
+    disabled: true,
+  },
+};
+
+export const ReadOnly: Story = {
+  args: {
+    label: "Read only",
+    value: "012345",
+    readOnly: true,
+  },
+};
+
+export const Focus: Story = {
+  args: {
+    label: "Focus",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const input = canvas.getByRole("textbox") as HTMLInputElement;
+    input.focus();
+
+    // Wait for the next tick
+    await new Promise((resolve) => setTimeout(resolve, 0));
+
+    // Add an identifier post interactions is complete
+    input.classList.add("percy-selector-placeholder");
+  },
+  parameters: {
+    percy: {
+      waitForSelector: ".percy-selector-placeholder",
+    },
+  },
+};
+
+export const Invalid: Story = {
+  args: {
+    label: "Invalid",
+    invalid: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const input = canvas.getByRole("textbox") as HTMLInputElement;
+    input.value = "Invalid";
+
+    // Wait for the next tick
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    // radix-ui always focuses the input when invalid, which is not what we want for snapshots
+    input.blur();
+
+    // Add an identifier post interactions is complete
+    input.classList.add("percy-selector-placeholder");
+  },
+  parameters: {
+    percy: {
+      waitForSelector: ".percy-selector-placeholder",
+    },
+  },
+};
--- src/components/Form/Controls/MFA/MFA.tsx
+++ src/components/Form/Controls/MFA/MFA.tsx
@@ -14,66 +14,98 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React, { forwardRef, PropsWithoutRef, useState } from "react";
-import classnames from "classnames";
-import { Control } from "../../Control";
+import React, { forwardRef, PropsWithoutRef, useLayoutEffect } from "react";
+import * as Form from "@radix-ui/react-form";
 
 import styles from "./MFA.module.css";
-import formStyles from "../../form.module.css";
 import classNames from "classnames";
+
+type DigitProp = {
+  invalid: boolean;
+  filled: boolean;
+  selected: boolean;
+};
+
+const Digit: React.FC<DigitProp> = ({ invalid, filled, selected }) => (
+  <div
+    className={styles.digit}
+    aria-hidden="true"
+    data-invalid={invalid ? "" : undefined}
+    data-filled={filled ? "" : undefined}
+    data-selected={selected ? "" : undefined}
+  />
+);
 
 type MFAProps = {
   className?: string;
   length?: number;
   value?: string;
   disabled?: boolean;
-} & React.ComponentProps<typeof Control>;
+} & React.ComponentProps<typeof Form.Control>;
 
-/**
- * Thin wrapper around Radix UI Control component
- * https://www.radix-ui.com/docs/primitives/components/form#control
- */
 export const MFAControl = forwardRef(function MFAControl(
   { className, length = 6, ...props }: PropsWithoutRef<MFAProps>,
   ref: React.ForwardedRef<HTMLInputElement>,
 ) {
   const classes = classNames(styles.mfa, className);
+  const [currentLength, setCurrentLength] = React.useState(0);
+  const [selection, setSelection] = React.useState<null | [number, number]>(
+    null,
+  );
 
-  const [mfaDigits, setMfaDigits] = useState(props.value ?? "");
+  const updateSelection = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const input = event.currentTarget;
+    if (
+      document.activeElement !== input ||
+      input.selectionStart === null ||
+      input.selectionEnd === null
+    ) {
+      setSelection(null);
+    } else {
+      setSelection([input.selectionStart, input.selectionEnd]);
+    }
+  };
 
   return (
     <div className={classes}>
-      <input
+      <Form.Control
         {...props}
+        inputMode="numeric"
         // Showing digits on mobile browsers. Using numbers is not really suited
         // as it often adds a way to increment or decrement the current value
         // which is not interesting for this use case
-        type="tel"
+        type="text"
         minLength={0}
         maxLength={length}
-        value={mfaDigits}
         className={styles["mfa-input"]}
-        onChange={(e) => {
-          setMfaDigits(
-            e.currentTarget.value.replace(/[^\d]/g, "").slice(0, length),
-          );
-        }}
-        autoComplete="off"
+        pattern={`\\d{${length}}`}
+        autoComplete="one-time-code"
+        onSelect={updateSelection}
+        onFocus={updateSelection}
+        onBlur={updateSelection}
+        onMouseDown={updateSelection}
+        onMouseMove={updateSelection}
+        onMouseUp={updateSelection}
         ref={ref}
+        onChange={(event) => {
+          updateSelection(event);
+          setCurrentLength(event.currentTarget.value?.length);
+        }}
       />
-      {Array(length)
-        .fill(0)
-        .map((_, index) => {
-          return (
-            <div
-              key={`digit-${index}`}
-              className={classnames(styles.digit, formStyles.control)}
-              aria-hidden="true"
-            >
-              {mfaDigits.charAt(index) ?? ""}
-            </div>
-          );
-        })}
+      <Form.ValidityState>
+        {(validity?: ValidityState) =>
+          Array.from(Array(length).keys()).map((index) => (
+            <Digit
+              key={index}
+              invalid={!!validity && !validity.valid}
+              filled={index < currentLength}
+              selected={
+                !!selection && index >= selection[0] && index < selection[1]
+              }
+            />
+          ))
+        }
+      </Form.ValidityState>
     </div>
   );
 });
