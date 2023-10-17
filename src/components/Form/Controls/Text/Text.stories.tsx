9ccb3b7bca3e98e7d43dba5e69d30f8fb955f5a7
Quentin Gliech <quentingliech@gmail.com>
2023-10-17 14:41:40 +0200

    Form: standalone Text control

---
+++ src/components/Form/Controls/Text/Text.stories.tsx
@@ -0,0 +1,140 @@
+/*
+Copyright 2023 The Matrix.org Foundation C.I.C.
+Copyright 2023 New Vector Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React from "react";
+
+import { Meta, StoryObj } from "@storybook/react";
+
+import { TextInput } from "./";
+
+type Props = { invalid?: boolean } & React.ComponentProps<typeof TextInput>;
+
+export default {
+  title: "Form/Controls/Text",
+  component: TextInput,
+  tags: ["autodocs"],
+  parameters: {
+    controls: {
+      include: [
+        "defaultValue",
+        "placeholder",
+        "disabled",
+        "autoFocus",
+        "readOnly",
+        "dataInvalid",
+      ],
+    },
+  },
+  argTypes: {
+    defaultValue: {
+      type: "string",
+    },
+    placeholder: {
+      type: "string",
+    },
+    disabled: {
+      type: "boolean",
+    },
+    autoFocus: {
+      type: "boolean",
+    },
+    readOnly: {
+      type: "boolean",
+    },
+    invalid: {
+      type: "boolean",
+    },
+  },
+  render: ({ invalid, ...restArgs }) => (
+    <TextInput data-invalid={invalid || undefined} {...restArgs} />
+  ),
+  args: {
+    placeholder: "",
+    autoFocus: false,
+    disabled: false,
+    readOnly: false,
+    invalid: false,
+  },
+} satisfies Meta<Props>;
+
+type Story = StoryObj<Props>;
+
+export const Empty: Story = {
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=792-2689",
+    },
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    defaultValue: "Filled",
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=792-2724",
+    },
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    defaultValue: "Disabled",
+    disabled: true,
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=792-2717",
+    },
+  },
+};
+
+export const ReadOnly: Story = {
+  args: {
+    defaultValue: "Read only",
+    readOnly: true,
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=2126-21865",
+    },
+  },
+};
+
+export const Focused: Story = {
+  args: {
+    autoFocus: true,
+  },
+};
+
+export const Invalid: Story = {
+  args: {
+    defaultValue: "Invalid",
+    invalid: true,
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=792-2710",
+    },
+  },
+};
