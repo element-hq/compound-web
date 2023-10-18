95f85d7a1c10c95eb83742dd116bfa7841534bff
Robin <robin@robin.town>
2023-10-17 23:48:24 -0400

    Add drawer menu component
    As an intermediate step towards implementing menus, this adds a component for the variant of menus used on mobile. To make it ready for external use, I still need to package it up in a more high-level component that also handles menu triggers.

---
+++ src/components/Menu/DrawerMenu.tsx
@@ -0,0 +1,56 @@
+/*
+Copyright 2023 New Vector Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import React, { ComponentPropsWithoutRef, ReactNode, forwardRef } from "react";
+import styles from "./DrawerMenu.module.css";
+import { platform } from "../../utils/platform";
+import classNames from "classnames";
+
+interface Props extends ComponentPropsWithoutRef<"div"> {
+  /**
+   * The menu title.
+   */
+  title: string;
+  /**
+   * The menu contents.
+   */
+  children: ReactNode;
+}
+
+/**
+ * A menu in a drawer, as commonly seen on mobile.
+ */
+// This an internal component not intended for export! Consumers should use it
+// via the Menu or ContextMenu components.
+export const DrawerMenu = forwardRef<HTMLDivElement, Props>(
+  ({ title, children, className, ...props }, ref): ReactNode => (
+    <div
+      ref={ref}
+      className={classNames(className, styles.drawer)}
+      aria-label={title}
+      data-platform={platform}
+      {...props}
+      role="menu"
+    >
+      <div className={styles.body}>
+        <div role="presentation" className={styles.handle} />
+        {children}
+      </div>
+    </div>
+  ),
+);
+
+DrawerMenu.displayName = "DrawerMenu";
