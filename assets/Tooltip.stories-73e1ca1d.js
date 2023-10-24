ead5b0163a21d8b199347232649407144b5c4e0c
RiotRobot <RiotRobot@users.noreply.github.com>
2023-10-24 14:58:24 +0000

    deploy: 3a9c0f5b6f871460ee5e19336ca80c9600c5d41e

---
+++ assets/Tooltip.stories-73e1ca1d.js
@@ -0,0 +1,49 @@
+import{j as o}from"./jsx-runtime-ffb262ed.js";import{T as n}from"./Tooltip-79ab42c1.js";import{I as e}from"./IconButton-0ce45897.js";import{S as t}from"./user-profile-4fb7f806.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./index-c339dd50.js";import"./index-d3ea75b5.js";import"./index-e131923d.js";const y={title:"Tooltip",component:n,tags:["autodocs"],argTypes:{},args:{}},d=()=>o.jsxs("div",{style:{display:"flex",gap:"50px",flexDirection:"column",alignItems:"center"},children:[o.jsx(n,{open:!0,side:"top",label:"@bob:example.org",children:o.jsx(e,{children:o.jsx(t,{})})}),o.jsx(n,{open:!0,side:"right",label:"@bob:example.org",children:o.jsx(e,{children:o.jsx(t,{})})}),o.jsx(n,{open:!0,side:"bottom",label:"@bob:example.org",children:o.jsx(e,{children:o.jsx(t,{})})}),o.jsx(n,{open:!0,side:"left",label:"@bob:example.org",children:o.jsx(e,{children:o.jsx(t,{})})})]}),l=d.bind({});l.args={};const x=()=>o.jsxs("div",{style:{display:"flex",gap:"50px",flexDirection:"column",alignItems:"center"},children:[o.jsx(n,{open:!0,align:"center",label:"Copy",caption:"⌘ + C",children:o.jsx(e,{children:o.jsx(t,{})})}),o.jsx(n,{open:!0,align:"start",label:"@bob:example.org",caption:"⌘ + C",children:o.jsx(e,{children:o.jsx(t,{})})}),o.jsx(n,{open:!0,align:"end",label:"@bob:example.org",caption:"⌘ + C",children:o.jsx(e,{children:o.jsx(t,{})})})]}),r=x.bind({});r.args={};var p,i,s;l.parameters={...l.parameters,docs:{...(p=l.parameters)==null?void 0:p.docs,source:{originalSource:`() => <div style={{
+  display: "flex",
+  gap: "50px",
+  flexDirection: "column",
+  alignItems: "center"
+}}>
+    <TooltipComponent open={true} side="top" label="@bob:example.org">
+      <IconButton>
+        <UserIcon />
+      </IconButton>
+    </TooltipComponent>
+    <TooltipComponent open={true} side="right" label="@bob:example.org">
+      <IconButton>
+        <UserIcon />
+      </IconButton>
+    </TooltipComponent>
+    <TooltipComponent open={true} side="bottom" label="@bob:example.org">
+      <IconButton>
+        <UserIcon />
+      </IconButton>
+    </TooltipComponent>
+    <TooltipComponent open={true} side="left" label="@bob:example.org">
+      <IconButton>
+        <UserIcon />
+      </IconButton>
+    </TooltipComponent>
+  </div>`,...(s=(i=l.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var a,c,m;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`() => <div style={{
+  display: "flex",
+  gap: "50px",
+  flexDirection: "column",
+  alignItems: "center"
+}}>
+    <TooltipComponent open={true} align="center" label="Copy" caption="⌘ + C">
+      <IconButton>
+        <UserIcon />
+      </IconButton>
+    </TooltipComponent>
+    <TooltipComponent open={true} align="start" label="@bob:example.org" caption="⌘ + C">
+      <IconButton>
+        <UserIcon />
+      </IconButton>
+    </TooltipComponent>
+    <TooltipComponent open={true} align="end" label="@bob:example.org" caption="⌘ + C">
+      <IconButton>
+        <UserIcon />
+      </IconButton>
+    </TooltipComponent>
+  </div>`,...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const U=["Side","Align"];export{r as Align,l as Side,U as __namedExportsOrder,y as default};
+//# sourceMappingURL=Tooltip.stories-73e1ca1d.js.map
