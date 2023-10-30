49190345ecd180b93a1fc25bcbd5e26cc30c52a7
Kerry Archibald <kerrya@element.io>
2023-10-30 15:26:04 +1300

    add @storybook/addon-a11y

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,23 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
+const addons = [
+  "@storybook/addon-links",
+  "@storybook/addon-essentials",
+  "@storybook/addon-interactions",
+  "@storybook/addon-designs",
+  "@storybook/addon-themes",
+];
+
+if (process.env.NODE_ENV === "development") {
+  addons.push("@storybook/addon-a11y")
+}
+
 const config: StorybookConfig = {
   stories: [
     "../src/**/*.mdx",
     "../src/**/!(__ComponentTemplate__)*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-designs",
-    "@storybook/addon-themes",
-  ],
+  addons,
   framework: {
     name: "@storybook/react-vite",
     options: {},
