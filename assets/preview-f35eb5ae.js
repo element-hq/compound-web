24f8f261af3317d61aa4bfb8b0653e37ae77653b
kerryarchibald <kerryarchibald@users.noreply.github.com>
2023-11-09 04:45:09 +0000

    deploy: c3cddd2588edcded32ae08fc5427ca49c996f261

--- assets/preview-f35eb5ae.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-f937882c.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-b192ad8e.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-b192ad8e.js","./iframe-f937882c.js","./index-8ee6c85d.js","./_commonjsHelpers-de833af9.js","./_getPrototype-cda9ef9d.js","./index-8bf7f4ad.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-f35eb5ae.js.map
