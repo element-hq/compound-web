65b94f93a9bad77c84dec5c3610ad7bbea48ebe1
robintown <robintown@users.noreply.github.com>
2024-01-10 14:22:12 +0000

    deploy: c5d37688659085d0c19873ed904d0bf0aac864d6

---
+++ assets/TooltipProvider-174de378.js
@@ -0,0 +1,3 @@
+import{$ as t}from"./Tooltip-8183383b.js";const o=t;try{o.displayName="TooltipProvider",o.__docgenInfo={description:`Provides global functionality to your tooltips. You must wrap your
+application in this component for tooltips to function.`,displayName:"TooltipProvider",props:{}}}catch{}export{o as T};
+//# sourceMappingURL=TooltipProvider-174de378.js.map
---
+++ components/Tooltip/TooltipProvider.d.ts
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from "react";
+interface TooltipProviderProps {
+    children: ReactNode;
+}
+/**
+ * Provides global functionality to your tooltips. You must wrap your
+ * application in this component for tooltips to function.
+ */
+export declare const TooltipProvider: FC<TooltipProviderProps>;
+export {};
+//# sourceMappingURL=TooltipProvider.d.ts.map
