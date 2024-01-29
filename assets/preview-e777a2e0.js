d127d2f2b9f883e9f65d4d98aecbc2225ada88cb
dbkr <dbkr@users.noreply.github.com>
2024-01-29 14:52:12 +0000

    deploy: 99be13cb2e8b4e563f0781e9b4e42f45aaa03618

---
+++ assets/preview-e777a2e0.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-653ca1a4.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-5afde979.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-5afde979.js","./iframe-653ca1a4.js","./index-8ee6c85d.js","./_commonjsHelpers-de833af9.js","./_getPrototype-cda9ef9d.js","./index-8bf7f4ad.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-e777a2e0.js.map
