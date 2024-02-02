d9bb03c5508368aa06e67a99ad3c2d400d0232db
robintown <robintown@users.noreply.github.com>
2024-02-02 14:41:36 +0000

    deploy: aee9da51daa3c8d7d8e41102e6c722cc35d7e84e

---
+++ assets/MenuItem.stories-9d76d089.js
@@ -0,0 +1,59 @@
+import{j as e}from"./jsx-runtime-f6d73c06.js";import{S as B}from"./extensions-37e27587.js";import{C as U}from"./chat-ae65e692.js";import"./index-8ee6c85d.js";import{M as d}from"./MenuItem-88b28948.js";import{T as p}from"./Text-270962e1.js";import"./_commonjsHelpers-de833af9.js";import"./index-e131923d.js";import"./index-fc9b93e8.js";import"./Typography-1dd46c86.js";const N=r=>e.jsxs("svg",{xmlns:"http://www.w3.org/2000/svg",width:"1em",height:"1em",fill:"currentColor",viewBox:"0 0 24 24",className:"cpd-icon",...r,children:[e.jsx("path",{d:"M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm-2 0a2 2 0 1 0-4 0 2 2 0 0 0 4 0Z"}),e.jsx("path",{d:"M11.312 2h1.376A2.312 2.312 0 0 1 15 4.312v.247l.002.003c.01.014.031.033.064.047.03.013.056.013.07.01h.002l.177-.177a2.312 2.312 0 0 1 3.27 0l.973.974a2.312 2.312 0 0 1 0 3.269l-.177.177v.003a.134.134 0 0 0 .01.07.153.153 0 0 0 .047.063l.003.002h.247A2.312 2.312 0 0 1 22 11.312v1.376A2.312 2.312 0 0 1 19.688 15h-.247l-.003.002a.152.152 0 0 0-.047.064.134.134 0 0 0-.01.07v.002l.177.177a2.312 2.312 0 0 1 0 3.27l-.974.973a2.312 2.312 0 0 1-3.269 0l-.177-.177h-.003a.134.134 0 0 0-.07.01.152.152 0 0 0-.063.047l-.002.003v.247A2.312 2.312 0 0 1 12.688 22h-1.376A2.312 2.312 0 0 1 9 19.688v-.247l-.002-.003a.153.153 0 0 0-.064-.047.134.134 0 0 0-.07-.01h-.002l-.177.177a2.312 2.312 0 0 1-3.27 0l-.973-.974a2.312 2.312 0 0 1 0-3.269l.177-.177v-.003a.135.135 0 0 0-.01-.07.152.152 0 0 0-.047-.063L4.559 15h-.247A2.312 2.312 0 0 1 2 12.688v-1.376A2.312 2.312 0 0 1 4.312 9h.247l.003-.002a.153.153 0 0 0 .047-.064.135.135 0 0 0 .01-.07v-.002l-.177-.177a2.312 2.312 0 0 1 0-3.27l.974-.973a2.312 2.312 0 0 1 3.269 0l.177.177h.003a.135.135 0 0 0 .07-.01.153.153 0 0 0 .063-.047L9 4.559v-.247A2.312 2.312 0 0 1 11.312 2ZM11 4.312v.257c0 .893-.59 1.593-1.299 1.887-.716.297-1.622.21-2.248-.418l-.182-.182a.312.312 0 0 0-.441 0l-.974.974a.312.312 0 0 0 0 .44l.182.183c.627.626.715 1.531.418 2.248C6.162 10.41 5.462 11 4.569 11h-.257a.312.312 0 0 0-.312.312v1.376c0 .172.14.312.312.312h.257c.893 0 1.593.59 1.887 1.299.297.716.21 1.622-.418 2.248l-.182.182a.312.312 0 0 0 0 .441l.974.973a.31.31 0 0 0 .44 0l.183-.181c.626-.627 1.532-.715 2.248-.418.709.294 1.299.994 1.299 1.887v.257c0 .172.14.312.312.312h1.376c.172 0 .312-.14.312-.312v-.257c0-.893.59-1.593 1.299-1.887.716-.297 1.622-.21 2.248.418l.182.181c.122.122.32.122.441 0l.973-.973a.312.312 0 0 0 0-.44l-.181-.183c-.627-.627-.715-1.532-.418-2.248.294-.709.994-1.299 1.887-1.299h.257c.172 0 .312-.14.312-.312v-1.376a.312.312 0 0 0-.312-.312h-.257c-.893 0-1.593-.59-1.887-1.299-.297-.717-.21-1.622.418-2.248l.181-.182a.312.312 0 0 0 0-.441l-.973-.974a.312.312 0 0 0-.44 0l-.183.182c-.627.627-1.532.715-2.248.418C13.59 6.162 13 5.462 13 4.569v-.257A.312.312 0 0 0 12.688 4h-1.376a.312.312 0 0 0-.312.312Z"})]}),ee={title:"Menu/MenuItem",component:d,tags:["autodocs"],argTypes:{onClick:{action:"clicked!"}},args:{label:"Menu item",children:e.jsx(p,{as:"span",size:"sm",children:"99"}),Icon:U},decorators:[r=>e.jsx("div",{style:{width:300},children:e.jsx(r,{})})]},o={render:()=>e.jsxs("div",{style:{width:300},children:[e.jsx(d,{Icon:U,label:"First item",onSelect:()=>{},children:e.jsx(p,{as:"span",size:"sm",children:"99"})}),e.jsx(d,{Icon:B,label:"Second item with a name that's quite long",onSelect:()=>{}}),e.jsx(d,{Icon:N,label:null,onSelect:()=>{},children:e.jsx(p,{as:"span",size:"sm",children:"Third item without a label"})})]})},a={args:{kind:"primary"}},s={args:{kind:"critical"}},t={args:{...a.args,disabled:!0}},n={args:{...s.args,disabled:!0}};function O(r){return e.jsx("div",{style:{height:24,width:48,backgroundColor:"teal"},...r})}const i={args:{...a.args,Icon:e.jsx(O,{})}},l={args:{...a.args,label:void 0}},c={args:{...a.args,label:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}},m={args:{...a.args,label:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, empor incididunt ut labore et dolore magna aliqua.",children:e.jsx(p,{as:"span",size:"sm",children:"Longer children too"})}};var u,g,h;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => <div style={{
+    width: 300
+  }}>
+      <MenuItemComponent Icon={ChatIcon} label="First item" onSelect={() => {}}>
+        <Text as="span" size="sm">
+          99
+        </Text>
+      </MenuItemComponent>
+      <MenuItemComponent Icon={ExtensionsIcon} label="Second item with a name that's quite long" onSelect={() => {}} />
+      <MenuItemComponent Icon={SettingsLabel} label={null} onSelect={() => {}}>
+        <Text as="span" size="sm">
+          Third item without a label
+        </Text>
+      </MenuItemComponent>
+    </div>
+}`,...(h=(g=o.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var b,x,v;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    kind: "primary"
+  }
+}`,...(v=(x=a.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};var C,I,S;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    kind: "critical"
+  }
+}`,...(S=(I=s.parameters)==null?void 0:I.docs)==null?void 0:S.source}}};var L,j,y;t.parameters={...t.parameters,docs:{...(L=t.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    disabled: true
+  }
+}`,...(y=(j=t.parameters)==null?void 0:j.docs)==null?void 0:y.source}}};var A,w,M;n.parameters={...n.parameters,docs:{...(A=n.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    ...Critical.args,
+    disabled: true
+  }
+}`,...(M=(w=n.parameters)==null?void 0:w.docs)==null?void 0:M.source}}};var q,T,f;i.parameters={...i.parameters,docs:{...(q=i.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    Icon: <SimpleComponent />
+  }
+}`,...(f=(T=i.parameters)==null?void 0:T.docs)==null?void 0:f.source}}};var P,k,z;l.parameters={...l.parameters,docs:{...(P=l.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    label: undefined
+  }
+}`,...(z=(k=l.parameters)==null?void 0:k.docs)==null?void 0:z.source}}};var E,W,D;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    label: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }
+}`,...(D=(W=c.parameters)==null?void 0:W.docs)==null?void 0:D.source}}};var Z,_,F;m.parameters={...m.parameters,docs:{...(Z=m.parameters)==null?void 0:Z.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    label: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, empor incididunt ut labore et dolore magna aliqua.",
+    children: <Text as="span" size="sm">
+        Longer children too
+      </Text>
+  }
+}`,...(F=(_=m.parameters)==null?void 0:_.docs)==null?void 0:F.source}}};const ae=["Example","Primary","Critical","PrimaryDisabled","CriticalDisabled","IconIsComponent","WithoutLabel","WithALongLabel","WithALongLabelAndChildren"];export{s as Critical,n as CriticalDisabled,o as Example,i as IconIsComponent,a as Primary,t as PrimaryDisabled,c as WithALongLabel,m as WithALongLabelAndChildren,l as WithoutLabel,ae as __namedExportsOrder,ee as default};
+//# sourceMappingURL=MenuItem.stories-9d76d089.js.map
---
+++ assets/close-e61dae86.js
@@ -0,0 +1,2 @@
+import{j as s}from"./jsx-runtime-f6d73c06.js";import"./index-8ee6c85d.js";const e=o=>s.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",width:"1em",height:"1em",fill:"currentColor",viewBox:"0 0 24 24",className:"cpd-icon",...o,children:s.jsx("path",{d:"M6.293 6.293a1 1 0 0 1 1.414 0L12 10.586l4.293-4.293a1 1 0 1 1 1.414 1.414L13.414 12l4.293 4.293a1 1 0 0 1-1.414 1.414L12 13.414l-4.293 4.293a1 1 0 0 1-1.414-1.414L10.586 12 6.293 7.707a1 1 0 0 1 0-1.414Z"})});export{e as S};
+//# sourceMappingURL=close-e61dae86.js.map
