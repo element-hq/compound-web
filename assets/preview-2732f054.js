9630a70578cf48420f2f16191092a4ca451c2e42
florianduros <florianduros@users.noreply.github.com>
2024-02-07 14:38:04 +0000

    deploy: 20c6f910fa78165cb3697e65fffc96501d7483c1

--- assets/preview-2732f054.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-620f8dd0.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-e0967366.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-e0967366.js","./iframe-620f8dd0.js","./index-8ee6c85d.js","./_commonjsHelpers-de833af9.js","./_getPrototype-cda9ef9d.js","./index-8bf7f4ad.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-2732f054.js.map
