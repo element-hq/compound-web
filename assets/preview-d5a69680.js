561e2e2187abb5eda99defc6b2449b6e5d42ac63
RiotRobot <RiotRobot@users.noreply.github.com>
2024-05-15 08:50:19 +0000

    deploy: 14f035f053f4cc85aef2088a08bc69f295adfa87

---
+++ assets/preview-d5a69680.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-0ecfc481.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-19c7e51a.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-19c7e51a.js","./iframe-0ecfc481.js","./index-8ee6c85d.js","./_commonjsHelpers-de833af9.js","./index-75f6da4b.js","./index-8bf7f4ad.js","./_getPrototype-499fcaa7.js","./_basePickBy-f596d2ee.js","./index-d37d4223.js","./index-633d3215.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-d5a69680.js.map
