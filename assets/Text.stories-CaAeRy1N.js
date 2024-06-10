1bc059661a07e693513640995b794c5df52b2353
RiotRobot <RiotRobot@users.noreply.github.com>
2024-06-10 09:22:31 +0000

    deploy: 93401896d3d51faf73107a8e10e0ad377d06bdc8

---
+++ assets/Text.stories-CaAeRy1N.js
@@ -0,0 +1,5 @@
+import{T as o}from"./Text-CFqa5qYe.js";import"./jsx-runtime-DoxjgJx5.js";import"./index-Cu9bd8lq.js";import"./Typography-D4-afh9Z.js";import"./index-C2KoGaFq.js";const p={title:"Typography",component:o,argTypes:{size:{options:["xs","sm","md","lg"],control:{type:"inline-radio"}},weight:{options:["regular","medium","semibold"],control:{type:"inline-radio"}},children:{type:"string"}},args:{children:"The quick brown fox jumps over the lazy dog",size:"md",weight:"regular"}},r={},e={args:{children:"-> 1x2x3"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "-> 1x2x3"
+  }
+}`,...e.parameters?.docs?.source}}};const c=["Text","Ligatures"];export{e as Ligatures,r as Text,c as __namedExportsOrder,p as default};
