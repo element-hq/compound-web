a0134e86e2e026aef9e6da3c95644c8ca0cbb68c
t3chguy <t3chguy@users.noreply.github.com>
2024-06-24 22:47:48 +0000

    deploy: 0f3c68088b96e3088601a2f9764d0bcd5f4c4fa5

--- components/Search/Search.d.ts
+++ components/Search/Search.d.ts
@@ -1,4 +1,4 @@
-import React from '../../../node_modules/react';
+import React, { ComponentProps } from '../../../node_modules/react';
 import { Field } from "../Form";
 type SearchProps = {
     /**
@@ -14,14 +14,10 @@ type SearchProps = {
      * The field name.
      */
     name: React.ComponentProps<typeof Field>["name"];
-    /**
-     * Event handler called when the search changes.
-     */
-    onChange?: (e: React.ChangeEvent) => void;
-};
+} & Omit<ComponentProps<"input">, "id" | "type">;
 /**
  * A standalone search component
  */
-export declare const Search: ({ className, onChange, placeholder, name, }: SearchProps) => JSX.Element;
+export declare const Search: React.ForwardRefExoticComponent<Omit<SearchProps, "ref"> & React.RefAttributes<HTMLInputElement>>;
 export {};
 //# sourceMappingURL=Search.d.ts.map
