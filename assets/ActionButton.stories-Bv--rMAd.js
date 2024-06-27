c66adafe69904db8211b5b11285aa77b9b0cd858
sandhose <sandhose@users.noreply.github.com>
2024-06-27 16:34:12 +0000

    deploy: cf2f5897ae815fccfedc7d7f874e82f7b992e156

---
+++ assets/ActionButton.stories-Bv--rMAd.js
@@ -0,0 +1,112 @@
+import{j as f}from"./jsx-runtime-DoxjgJx5.js";import{f as B}from"./index-B-6fpdcS.js";import{S as k}from"./check-CXsFGzWm.js";import{S as y}from"./close-D55JIkKE.js";import{S as C,a as v,b as h,c as O,d as E,e as z,f as V,g as b,h as M,i as T,j as x}from"./video-call-solid-DZxiZyaz.js";import{S as A}from"./search-CWkX64tk.js";import{S as F}from"./visibility-on-BrkRTjSX.js";import{B as U}from"./Button-Yybe91ei.js";import"./Alert-D-MUtyoA.js";import"./Avatar-D_h5K73j.js";import"./AvatarStack-zKeD_o_3.js";import"./Badge-C2aKwjRk.js";import"./IconButton-GCNuw6ew.js";import"./Search-Dco_6vuE.js";import"./Text-BrcN_buA.js";import"./ContextMenu-DezHmrk0.js";import"./Glass-CwZVZZDo.js";import"./Heading-D0UD_l3r.js";import"./IndicatorIcon-iinDG_Nf.js";import"./Link-DCPbMbAN.js";import"./Menu-BVPTPdCk.js";import"./MenuItem-BNN94JoD.js";import"./Separator-D0l5BZ7R.js";import"./ToggleMenuItem-IrFGmotN.js";import{T as _}from"./Tooltip-BpTSfUbF.js";import"./ReleaseAnnouncement-kJXVjrNP.js";import"./Toast-DiTTZX7x.js";import"./Action-BJniBKiJ.js";import"./Password-BL0bK9Cg.js";import"./Text-Cr8eyvvV.js";import"./MFA-Bg-QQaLT.js";import"./Checkbox-UWXrg9ui.js";import"./Radio-DnV3u_Pm.js";import"./Toggle-CCZdSH3V.js";/* empty css               */import"./index-Cu9bd8lq.js";import"./index-CVRyq5ci.js";import"./index-C2KoGaFq.js";import"./UnstyledButton-CUaD5tB8.js";import"./Typography-BcgMUEGb.js";import"./index-BEd8YfQ8.js";import"./index-Cmw95SmN.js";import"./index-tFWdirwu.js";import"./index-BHpG8fra.js";import"./index-s2Ee__EW.js";import"./InlineSpinner-CZEeBvQo.js";import"./useId-D5m2XpUu.js";import"./index-H3MR2rqV.js";import"./index-ChiipFpT.js";import"./FloatingMenu-Eb22vTDs.js";import"./DrawerMenu-BYj777Pr.js";import"./floating-ui.react-D5UUZX8q.js";const D=({label:L,...j})=>f.jsx(_,{label:L,children:f.jsx(U,{iconOnly:!0,...j})}),_r={title:"Button/Actions",component:D,tags:["autodocs"],argTypes:{label:{type:"string"},size:{options:["sm","lg"],control:{type:"inline-radio"}},kind:{options:["primary","secondary","tertiary"],control:{type:"inline-radio"}},destructive:{type:"boolean"},disabled:{type:"boolean"},as:{options:["a","button"],control:{type:"inline-radio"}},Icon:{options:["CheckIcon","CloseIcon","EditIcon","EndCallIcon","ExpandIcon","LinkIcon","MicOnSolidIcon","MicOffSolidIcon","SearchIcon","SettingsSolidIcon","ShareScreenSolidIcon","UserAddIcon","VideoCallOffSolidIcon","VideoCallSolidIcon","VisibilityOnIcon"],mapping:{CheckIcon:k,CloseIcon:y,EditIcon:C,EndCallIcon:v,ExpandIcon:h,LinkIcon:O,MicOnSolidIcon:E,MicOffSolidIcon:z,SearchIcon:A,SettingsSolidIcon:V,ShareScreenSolidIcon:b,UserAddIcon:M,VideoCallOffSolidIcon:T,VideoCallSolidIcon:x,VisibilityOnIcon:F},control:{type:"inline-radio"}},onClick:{action:"onClick"}},args:{label:"Action",size:"lg",as:"button",destructive:!1,disabled:!1,onClick:B()},controls:{exclude:["children"]}},r={args:{label:"Settings",kind:"secondary",Icon:V}},e={args:{label:"Copy link",kind:"secondary",Icon:O}},o={args:{label:"Turn video off",kind:"secondary",Icon:x}},n={args:{label:"Turn video on",kind:"primary",Icon:T}},a={args:{label:"Turn mic off",kind:"secondary",Icon:E}},s={args:{label:"Turn mic on",kind:"primary",Icon:z}},i={args:{label:"Share screen",kind:"secondary",Icon:b}},c={args:{label:"Stop sharing screen",kind:"primary",Icon:b}},t={args:{label:"Full screen",kind:"secondary",Icon:h}},l={args:{label:"End call",kind:"primary",destructive:!0,Icon:v}},d={args:{label:"Invite",kind:"primary",size:"sm",Icon:M}},m={args:{label:"Edit",kind:"primary",size:"sm",Icon:C}},p={args:{label:"Search",kind:"secondary",size:"sm",Icon:A}},S={args:{label:"Save",kind:"primary",size:"sm",Icon:k}},g={args:{label:"Save",kind:"primary",size:"sm",Icon:k,disabled:!0}},I={args:{label:"Cancel",kind:"secondary",size:"sm",Icon:y}},u={args:{label:"Close",kind:"secondary",size:"sm",Icon:y}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Settings",
+    kind: "secondary",
+    Icon: SettingsSolidIcon
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Copy link",
+    kind: "secondary",
+    Icon: LinkIcon
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Turn video off",
+    kind: "secondary",
+    Icon: VideoCallSolidIcon
+  }
+}`,...o.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Turn video on",
+    kind: "primary",
+    Icon: VideoCallOffSolidIcon
+  }
+}`,...n.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Turn mic off",
+    kind: "secondary",
+    Icon: MicOnSolidIcon
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Turn mic on",
+    kind: "primary",
+    Icon: MicOffSolidIcon
+  }
+}`,...s.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Share screen",
+    kind: "secondary",
+    Icon: ShareScreenSolidIcon
+  }
+}`,...i.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Stop sharing screen",
+    kind: "primary",
+    Icon: ShareScreenSolidIcon
+  }
+}`,...c.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Full screen",
+    kind: "secondary",
+    Icon: ExpandIcon
+  }
+}`,...t.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "End call",
+    kind: "primary",
+    destructive: true,
+    Icon: EndCallIcon
+  }
+}`,...l.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Invite",
+    kind: "primary",
+    size: "sm",
+    Icon: UserAddIcon
+  }
+}`,...d.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Edit",
+    kind: "primary",
+    size: "sm",
+    Icon: EditIcon
+  }
+}`,...m.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Search",
+    kind: "secondary",
+    size: "sm",
+    Icon: SearchIcon
+  }
+}`,...p.parameters?.docs?.source}}};S.parameters={...S.parameters,docs:{...S.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Save",
+    kind: "primary",
+    size: "sm",
+    Icon: CheckIcon
+  }
+}`,...S.parameters?.docs?.source}}};g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Save",
+    kind: "primary",
+    size: "sm",
+    Icon: CheckIcon,
+    disabled: true
+  }
+}`,...g.parameters?.docs?.source}}};I.parameters={...I.parameters,docs:{...I.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Cancel",
+    kind: "secondary",
+    size: "sm",
+    Icon: CloseIcon
+  }
+}`,...I.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Close",
+    kind: "secondary",
+    size: "sm",
+    Icon: CloseIcon
+  }
+}`,...u.parameters?.docs?.source}}};const Dr=["Settings","ShareLink","VideoOn","VideoOff","MicOn","MicOff","ShareScreen","SharingScreen","Fullscreen","EndCall","Invite","Edit","Search","Save","SaveDisabled","Cancel","Close"];export{I as Cancel,u as Close,m as Edit,l as EndCall,t as Fullscreen,d as Invite,s as MicOff,a as MicOn,S as Save,g as SaveDisabled,p as Search,r as Settings,e as ShareLink,i as ShareScreen,c as SharingScreen,n as VideoOff,o as VideoOn,Dr as __namedExportsOrder,_r as default};
