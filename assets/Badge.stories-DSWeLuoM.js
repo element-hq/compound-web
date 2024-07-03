7bf2975743cb35f0ae573f0b8f2b47f83c698535
RiotRobot <RiotRobot@users.noreply.github.com>
2024-07-03 09:37:01 +0000

    deploy: 4956e34ddb8e71854bf77edba8eddb6ddbd51ddb

--- assets/Badge.stories-DSWeLuoM.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./jsx-runtime-DoxjgJx5.js";import{B as t}from"./Badge-DhRzaND9.js";import"./index-Cu9bd8lq.js";import"./index-C2KoGaFq.js";import"./Typography-D-74lRMS.js";const c={title:"Badge",component:t,argTypes:{},args:{}},n=()=>e.jsxs(e.Fragment,{children:[e.jsx(t,{kind:"success",children:"Trusted"})," ",e.jsx(t,{kind:"critical",children:"Not trusted"})," ",e.jsx(t,{kind:"default",children:"Public room"})]}),o=n.bind({});o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => <>
-    <BadgeComponent kind="success">Trusted</BadgeComponent>
-    &nbsp;
-    <BadgeComponent kind="critical">Not trusted</BadgeComponent>
-    &nbsp;
-    <BadgeComponent kind="default">Public room</BadgeComponent>
-  </>`,...o.parameters?.docs?.source}}};const m=["Badge"];export{o as Badge,m as __namedExportsOrder,c as default};
---
+++ assets/Menu.stories-B6DWwITK.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-DoxjgJx5.js";import{r as l}from"./index-Cu9bd8lq.js";import{S as p}from"./user-profile-CB1AInEY.js";import{S as m,a as u,b as g}from"./leave-ZJppWO3C.js";import{M as r}from"./Menu-6-qq6z8w.js";import{M as o}from"./MenuItem-C3kYcHjL.js";import{S}from"./Separator-D8gjKPDD.js";import{B as I}from"./Button-BPkQI5Wr.js";import"./index-CjTrpfS1.js";import"./index-B2VpcjXg.js";import"./index-s2Ee__EW.js";import"./extends-DhVGKg3b.js";import"./index-_X4q1Xs8.js";import"./FloatingMenu-Eb22vTDs.js";import"./index-C2KoGaFq.js";import"./useId-D5m2XpUu.js";import"./Text-BrcN_buA.js";import"./Typography-BcgMUEGb.js";import"./DrawerMenu-BYj777Pr.js";import"./floating-ui.react-DRrg5oqz.js";import"./UnstyledButton-CUaD5tB8.js";const W={title:"Menu",component:r,tags:["autodocs"],argTypes:{},args:{}},a=s=>{const[i,c]=l.useState(!0);return e.jsxs(r,{...s,open:i,onOpenChange:c,trigger:e.jsx(I,{children:"Open menu"}),align:"start",children:[e.jsx(o,{Icon:p,label:"Profile",onSelect:()=>{}}),e.jsx(o,{Icon:m,label:"Notifications",onSelect:()=>{}}),e.jsx(o,{Icon:u,label:"Feedback",onSelect:()=>{}}),e.jsx(S,{}),e.jsx(o,{kind:"critical",Icon:g,label:"Sign out",onSelect:()=>{}})]})},t=a.bind({});t.args={title:"Today's Menu"};const n=a.bind({});n.args={title:"Untitled Menu",showTitle:!1};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`args => {
+  const [open, setOpen] = useState(true);
+  return <MenuComponent {...args} open={open} onOpenChange={setOpen} trigger={<Button>Open menu</Button>} align="start">
+      <MenuItem Icon={UserProfileIcon} label="Profile" onSelect={() => {}} />
+      <MenuItem Icon={NotificationsIcon} label="Notifications" onSelect={() => {}} />
+      <MenuItem Icon={ChatProblemIcon} label="Feedback" onSelect={() => {}} />
+      <Separator />
+      <MenuItem kind="critical" Icon={LeaveIcon} label="Sign out" onSelect={() => {}} />
+    </MenuComponent>;
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`args => {
+  const [open, setOpen] = useState(true);
+  return <MenuComponent {...args} open={open} onOpenChange={setOpen} trigger={<Button>Open menu</Button>} align="start">
+      <MenuItem Icon={UserProfileIcon} label="Profile" onSelect={() => {}} />
+      <MenuItem Icon={NotificationsIcon} label="Notifications" onSelect={() => {}} />
+      <MenuItem Icon={ChatProblemIcon} label="Feedback" onSelect={() => {}} />
+      <Separator />
+      <MenuItem kind="critical" Icon={LeaveIcon} label="Sign out" onSelect={() => {}} />
+    </MenuComponent>;
+}`,...n.parameters?.docs?.source}}};const w=["Menu","WithoutTitle"];export{t as Menu,n as WithoutTitle,w as __namedExportsOrder,W as default};
