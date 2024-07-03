786276f19428edca9fd7dd20667c01e20e76314a
MidhunSureshR <MidhunSureshR@users.noreply.github.com>
2024-07-03 09:33:12 +0000

    deploy: e69dacc9ec45b197923d657556095184884a7f73

--- assets/Separator.stories-DYrnMOL2.js
+++ assets/Separator.stories-DYrnMOL2.js
@@ -1,4 +1,4 @@
-import{S as o}from"./Separator-DZCs-tFb.js";import"./jsx-runtime-DoxjgJx5.js";import"./index-Cu9bd8lq.js";import"./index-C2KoGaFq.js";import"./index-s2Ee__EW.js";import"./index-B2VpcjXg.js";import"./floating-ui.react-DRrg5oqz.js";const d={title:"Separator",component:o,tags:["autodocs"],argTypes:{},args:{}},r={args:{kind:"primary"}},a={args:{kind:"secondary"}},e={args:{kind:"section"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+import{S as o}from"./Separator-DoybTZWY.js";import"./jsx-runtime-DoxjgJx5.js";import"./index-Cu9bd8lq.js";import"./index-C2KoGaFq.js";import"./index-s2Ee__EW.js";import"./index-B2VpcjXg.js";import"./floating-ui.react-DRrg5oqz.js";const d={title:"Separator",component:o,tags:["autodocs"],argTypes:{},args:{}},r={args:{kind:"primary"}},a={args:{kind:"secondary"}},e={args:{kind:"section"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
   args: {
     kind: "primary"
   }
