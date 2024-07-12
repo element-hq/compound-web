81900cd8c58947e4dd23e219ad825cf12369c499
RiotRobot <RiotRobot@users.noreply.github.com>
2024-07-12 14:09:32 +0000

    deploy: 0d1c20db65a14a3b7aac605e5ffbfecf029f9434

---
+++ assets/Separator.stories-6lufygnf.js
@@ -0,0 +1,13 @@
+import{S as e}from"./Separator-cQ79fVAM.js";import"./jsx-runtime-DoxjgJx5.js";import"./index-Cu9bd8lq.js";import"./index-C2KoGaFq.js";import"./index-CEUj4GHJ.js";import"./index-s2Ee__EW.js";import"./index-B2VpcjXg.js";import"./floating-ui.react-D5UUZX8q.js";const g={title:"Separator",component:e,tags:["autodocs"],argTypes:{},args:{}},r={args:{kind:"primary"}},a={args:{kind:"secondary"}},o={args:{kind:"section"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "primary"
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "secondary"
+  }
+}`,...a.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "section"
+  }
+}`,...o.parameters?.docs?.source}}};const u=["Primary","Secondary","Section"];export{r as Primary,a as Secondary,o as Section,u as __namedExportsOrder,g as default};
