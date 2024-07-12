3c3c9e32d8052b16477dbd3eeb0995a28fafd700
sandhose <sandhose@users.noreply.github.com>
2024-07-12 12:32:58 +0000

    deploy: c06cfb35fb6ee858e4bb451c896a0549fd843f24

---
+++ components/Progress/Progress.d.ts
@@ -0,0 +1,27 @@
+import React from '../../../node_modules/react';
+type ProgressProps = {
+    /** The size variant of the progress bar */
+    size: "sm" | "lg";
+    /**
+     * The colour variant to use for the progress bar indicator
+     * If not set, the progress bar will be rendered with a gray tone, which should only be used when the progress bar is empty
+     */
+    tint?: "red" | "orange" | "lime" | "green";
+    /** The CSS class name forwarded to the root element */
+    className?: string;
+    /** The value of the progress bar. Defaults to max if not provided */
+    value?: number | null;
+    /** The maximum value of the progress bar. Defaults to 1 if not provided */
+    max?: number;
+    /**
+     * A function to get the text label to display in the progress bar.
+     * If set, it will display a label on top of the progress bar
+     */
+    getValueLabel?: (value: number, max: number) => string;
+} & Omit<React.ComponentProps<"div">, "children">;
+/**
+ * Displays an indicator showing the completion progress of a task, optionally with a label
+ */
+export declare const Progress: React.ForwardRefExoticComponent<Omit<ProgressProps, "ref"> & React.RefAttributes<HTMLDivElement>>;
+export {};
+//# sourceMappingURL=Progress.d.ts.map
--- index.d.ts
+++ index.d.ts
@@ -16,6 +16,7 @@ export { Link } from "./components/Link/Link";
 export { NavBar, NavItem } from "./components/Nav";
 export { Menu } from "./components/Menu/Menu";
 export { MenuItem } from "./components/Menu/MenuItem";
+export { Progress } from "./components/Progress/Progress";
 export { Search } from "./components/Search/Search";
 export { Separator } from "./components/Separator/Separator";
 export { ToggleMenuItem } from "./components/Menu/ToggleMenuItem";
