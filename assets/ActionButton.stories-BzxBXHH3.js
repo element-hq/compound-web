2dfa3622f9e74a5b97b58c2cf39ac413141bebfc
t3chguy <t3chguy@users.noreply.github.com>
2024-07-29 09:48:31 +0000

    deploy: c487773ac6fe51f1637234c1a403a0332fcbe08a

---
+++ assets/ActionButton.stories-BzxBXHH3.js
@@ -0,0 +1,112 @@
+import{j as y}from"./jsx-runtime-CexXSJP5.js";import{f as O}from"./index-BSM_bQPn.js";import{i as b,s as z,l as E,v as T,a as x,m as M,b as V,c as k,e as j,d as A,u as B,f as F}from"./index-DvDDli3q.js";import{B as L}from"./Button-iukP6Ype.js";import"./Alert-Bc7kq_vo.js";import"./Avatar-DyB-Y6ln.js";import"./AvatarStack-DBPldPV8.js";import"./Badge-Cf9Fivpi.js";import"./IconButton-BTwSTSqq.js";import"./Search-BeHIlM2F.js";import"./Text-BgCCN_GG.js";import"./ContextMenu-DTzt9ghB.js";import"./Glass-COqtBFQM.js";import"./Heading-DqG7uK5Y.js";import"./IndicatorIcon-Bk7xsnCi.js";import"./Link-DttFLitk.js";import"./NavItem-CCdVkP0k.js";import"./Menu-BDkXLAb-.js";import"./MenuItem-DTeHnbO2.js";import"./Progress-CTwHYCo_.js";import"./Separator-DsecY29E.js";import"./ToggleMenuItem-Byr46rko.js";import{T as _}from"./Tooltip-D3X7zSTe.js";import"./ReleaseAnnouncement-D5sKmLPC.js";import"./Toast-BFUH-DR1.js";import"./Action-CWepeJGG.js";import"./Password-CGBCIgRd.js";import"./Text-Cz5oMJIL.js";import"./MFA-QkHhz391.js";import"./Checkbox-C46Ejh3M.js";import"./Radio-CvV1BscZ.js";import"./Toggle-Q78m7eTC.js";/* empty css               */import{S as D}from"./search-BC9ffyhQ.js";import{C as f}from"./check-DrA_wPr-.js";import{C}from"./close-BapqxG5D.js";import"./index-BP8_t0zE.js";import"./index-C8k3Z-3Y.js";import"./notifications-CmnzXmCw.js";import"./extensions-DlgbNgpo.js";import"./chevron-right-CaLdGLa3.js";import"./settings-DxGsE2C2.js";import"./spinner-DszwWdC7.js";import"./threads-BMeJ5mkW.js";import"./user-profile-EvkA4CON.js";import"./visibility-on-CjnqbRsU.js";import"./warning-ChS4d0Jy.js";import"./index-CSpfAsmC.js";import"./UnstyledButton-C9fNLva5.js";import"./Typography-DuHxGDyc.js";import"./index-Dn9bhuYh.js";import"./index-DD7XHAoh.js";import"./index-Dp4tTk3o.js";import"./index-vl_XNHfj.js";import"./index-CkD-EKAR.js";import"./index-Cw6ikIwd.js";import"./InlineSpinner-DZ2uBgAt.js";import"./useId-DIHOOv91.js";import"./index-CTpxkypK.js";import"./floating-ui.react-B-qURRA4.js";import"./FloatingMenu-t36h7pay.js";import"./DrawerMenu-BgyYdqxq.js";const R=({label:h,...v})=>y.jsx(_,{label:h,children:y.jsx(L,{iconOnly:!0,...v})}),Pr={title:"Button/Actions",component:R,tags:["autodocs"],argTypes:{label:{type:"string"},size:{options:["sm","lg"],control:{type:"inline-radio"}},kind:{options:["primary","secondary","tertiary"],control:{type:"inline-radio"}},destructive:{type:"boolean"},disabled:{type:"boolean"},as:{options:["a","button"],control:{type:"inline-radio"}},Icon:{options:Object.keys(b),mapping:b,control:{type:"select"}},onClick:{action:"onClick"}},args:{label:"Action",size:"lg",as:"button",destructive:!1,disabled:!1,onClick:O()},controls:{exclude:["children"]}},r={args:{label:"Settings",kind:"secondary",Icon:z}},e={args:{label:"Copy link",kind:"secondary",Icon:E}},o={args:{label:"Turn video off",kind:"secondary",Icon:T}},n={args:{label:"Turn video on",kind:"primary",Icon:x}},s={args:{label:"Turn mic off",kind:"secondary",Icon:M}},a={args:{label:"Turn mic on",kind:"primary",Icon:V}},i={args:{label:"Share screen",kind:"secondary",Icon:k}},c={args:{label:"Stop sharing screen",kind:"primary",Icon:k}},t={args:{label:"Full screen",kind:"secondary",Icon:j}},m={args:{label:"End call",kind:"primary",destructive:!0,Icon:A}},l={args:{label:"Invite",kind:"primary",size:"sm",Icon:B}},d={args:{label:"Edit",kind:"primary",size:"sm",Icon:F}},p={args:{label:"Search",kind:"secondary",size:"sm",Icon:D}},u={args:{label:"Save",kind:"primary",size:"sm",Icon:f}},S={args:{label:"Save",kind:"primary",size:"sm",Icon:f,disabled:!0}},g={args:{label:"Cancel",kind:"secondary",size:"sm",Icon:C}},I={args:{label:"Close",kind:"secondary",size:"sm",Icon:C}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Settings",
+    kind: "secondary",
+    Icon: icons.SettingsSolidIcon
+  }
+}`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Copy link",
+    kind: "secondary",
+    Icon: icons.LinkIcon
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Turn video off",
+    kind: "secondary",
+    Icon: icons.VideoCallSolidIcon
+  }
+}`,...o.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Turn video on",
+    kind: "primary",
+    Icon: icons.VideoCallOffSolidIcon
+  }
+}`,...n.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Turn mic off",
+    kind: "secondary",
+    Icon: icons.MicOnSolidIcon
+  }
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Turn mic on",
+    kind: "primary",
+    Icon: icons.MicOffSolidIcon
+  }
+}`,...a.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Share screen",
+    kind: "secondary",
+    Icon: icons.ShareScreenSolidIcon
+  }
+}`,...i.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Stop sharing screen",
+    kind: "primary",
+    Icon: icons.ShareScreenSolidIcon
+  }
+}`,...c.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Full screen",
+    kind: "secondary",
+    Icon: icons.ExpandIcon
+  }
+}`,...t.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "End call",
+    kind: "primary",
+    destructive: true,
+    Icon: icons.EndCallIcon
+  }
+}`,...m.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Invite",
+    kind: "primary",
+    size: "sm",
+    Icon: icons.UserAddIcon
+  }
+}`,...l.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Edit",
+    kind: "primary",
+    size: "sm",
+    Icon: icons.EditIcon
+  }
+}`,...d.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Search",
+    kind: "secondary",
+    size: "sm",
+    Icon: icons.SearchIcon
+  }
+}`,...p.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Save",
+    kind: "primary",
+    size: "sm",
+    Icon: icons.CheckIcon
+  }
+}`,...u.parameters?.docs?.source}}};S.parameters={...S.parameters,docs:{...S.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Save",
+    kind: "primary",
+    size: "sm",
+    Icon: icons.CheckIcon,
+    disabled: true
+  }
+}`,...S.parameters?.docs?.source}}};g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Cancel",
+    kind: "secondary",
+    size: "sm",
+    Icon: icons.CloseIcon
+  }
+}`,...g.parameters?.docs?.source}}};I.parameters={...I.parameters,docs:{...I.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Close",
+    kind: "secondary",
+    size: "sm",
+    Icon: icons.CloseIcon
+  }
+}`,...I.parameters?.docs?.source}}};const Qr=["Settings","ShareLink","VideoOn","VideoOff","MicOn","MicOff","ShareScreen","SharingScreen","Fullscreen","EndCall","Invite","Edit","Search","Save","SaveDisabled","Cancel","Close"];export{g as Cancel,I as Close,d as Edit,m as EndCall,t as Fullscreen,l as Invite,a as MicOff,s as MicOn,u as Save,S as SaveDisabled,p as Search,r as Settings,e as ShareLink,i as ShareScreen,c as SharingScreen,n as VideoOff,o as VideoOn,Qr as __namedExportsOrder,Pr as default};
