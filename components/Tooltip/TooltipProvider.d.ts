7c251601dda7a4e4885d06d173db5e271448245e
robintown <robintown@users.noreply.github.com>
2024-08-02 17:11:29 +0000

    deploy: ea164d8bb02bad860ff39d1f42b61a2b4c22452c

---
+++ components/Tooltip/TooltipProvider.d.ts
@@ -0,0 +1,16 @@
+import { FC } from '../../../node_modules/react';
+
+export declare const hoverDelay: {
+    open: number;
+    close: number;
+};
+interface Props {
+    children: JSX.Element;
+}
+/**
+ * Enables tooltips to share a global "warm-up" period for their hover delay.
+ * You must wrap your application in this component for tooltips to function.
+ */
+export declare const TooltipProvider: FC<Props>;
+export {};
+//# sourceMappingURL=TooltipProvider.d.ts.map
