85bb1fe42dd112f4d720085a4349dac3b80ebe1d
t3chguy <t3chguy@users.noreply.github.com>
2024-08-28 10:20:42 +0000

    deploy: be5a8fdcde48fa2a9accc9e4e44f79f59b2527d6

--- assets/FloatingMenu.stories-CLMaeXHR.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./jsx-runtime-CexXSJP5.js";import{U as a}from"./user-profile-EvkA4CON.js";import{N as r,C as s,L as c}from"./notifications-MBLL4uPX.js";import{F as n}from"./FloatingMenu-t36h7pay.js";import{M as t}from"./MenuItem-CmF0V73j.js";import{S as l}from"./Separator-DLPJ0xlb.js";import"./index-BP8_t0zE.js";import"./index-CSpfAsmC.js";import"./useId-DIHOOv91.js";import"./Text-BgCCN_GG.js";import"./Typography-DuHxGDyc.js";import"./chevron-right-DF47_mtr.js";import"./index-Dp4tTk3o.js";import"./index-CkD-EKAR.js";import"./index-Cw6ikIwd.js";import"./floating-ui.react-KXiqSXy2.js";const h={title:"Menu/FloatingMenu",component:n,argTypes:{},args:{}},m=i=>o.jsxs(n,{...i,title:"Settings",children:[o.jsx(t,{Icon:a,label:"Profile",onSelect:()=>{}}),o.jsx(t,{Icon:r,label:"Notificationsnotificationsnotifications",onSelect:()=>{}}),o.jsx(t,{Icon:s,label:"Feedback",onSelect:()=>{}}),o.jsx(l,{}),o.jsx(t,{kind:"critical",Icon:c,label:"Sign out",onSelect:()=>{}})]}),e=m.bind({});e.args={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`args => <FloatingMenuComponent {...args} title="Settings">
-    <MenuItem Icon={UserProfileIcon} label="Profile" onSelect={() => {}} />
-    {/* Extra long label to demonstrate word breaking */}
-    <MenuItem Icon={NotificationsIcon} label="Notificationsnotificationsnotifications" onSelect={() => {}} />
-    <MenuItem Icon={ChatProblemIcon} label="Feedback" onSelect={() => {}} />
-    <Separator />
-    <MenuItem kind="critical" Icon={LeaveIcon} label="Sign out" onSelect={() => {}} />
-  </FloatingMenuComponent>`,...e.parameters?.docs?.source}}};const E=["FloatingMenu"];export{e as FloatingMenu,E as __namedExportsOrder,h as default};
---
+++ assets/Menu.stories-DpXR04mQ.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-CexXSJP5.js";import{r as l}from"./index-BP8_t0zE.js";import{U as p}from"./user-profile-EvkA4CON.js";import{N as m,C as u,L as I}from"./notifications-MBLL4uPX.js";import{M as r}from"./Menu-H7LLfzYQ.js";import{M as t}from"./MenuItem-CmF0V73j.js";import{S as g}from"./Separator-gTPqRITt.js";import{B as S}from"./Button-C0_Ps78g.js";import"./index-DD7XHAoh.js";import"./index-Dp4tTk3o.js";import"./index-vl_XNHfj.js";import"./index-DOPlf36I.js";import"./index-CkD-EKAR.js";import"./index-Cw6ikIwd.js";import"./floating-ui.react-KXiqSXy2.js";import"./FloatingMenu-t36h7pay.js";import"./index-CSpfAsmC.js";import"./useId-DIHOOv91.js";import"./Text-BgCCN_GG.js";import"./Typography-DuHxGDyc.js";import"./DrawerMenu-BgyYdqxq.js";import"./chevron-right-DF47_mtr.js";import"./UnstyledButton-C9fNLva5.js";const R={title:"Menu",component:r,tags:["autodocs"],argTypes:{},args:{}},a=s=>{const[i,c]=l.useState(!0);return e.jsxs(r,{...s,open:i,onOpenChange:c,trigger:e.jsx(S,{children:"Open menu"}),align:"start",children:[e.jsx(t,{Icon:p,label:"Profile",onSelect:()=>{}}),e.jsx(t,{Icon:m,label:"Notifications",onSelect:()=>{}}),e.jsx(t,{Icon:u,label:"Feedback",onSelect:()=>{}}),e.jsx(g,{}),e.jsx(t,{kind:"critical",Icon:I,label:"Sign out",onSelect:()=>{}})]})},n=a.bind({});n.args={title:"Today's Menu"};const o=a.bind({});o.args={title:"Untitled Menu",showTitle:!1};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`args => {
+  const [open, setOpen] = useState(true);
+  return <MenuComponent {...args} open={open} onOpenChange={setOpen} trigger={<Button>Open menu</Button>} align="start">
+      <MenuItem Icon={UserProfileIcon} label="Profile" onSelect={() => {}} />
+      <MenuItem Icon={NotificationsIcon} label="Notifications" onSelect={() => {}} />
+      <MenuItem Icon={ChatProblemIcon} label="Feedback" onSelect={() => {}} />
+      <Separator />
+      <MenuItem kind="critical" Icon={LeaveIcon} label="Sign out" onSelect={() => {}} />
+    </MenuComponent>;
+}`,...n.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`args => {
+  const [open, setOpen] = useState(true);
+  return <MenuComponent {...args} open={open} onOpenChange={setOpen} trigger={<Button>Open menu</Button>} align="start">
+      <MenuItem Icon={UserProfileIcon} label="Profile" onSelect={() => {}} />
+      <MenuItem Icon={NotificationsIcon} label="Notifications" onSelect={() => {}} />
+      <MenuItem Icon={ChatProblemIcon} label="Feedback" onSelect={() => {}} />
+      <Separator />
+      <MenuItem kind="critical" Icon={LeaveIcon} label="Sign out" onSelect={() => {}} />
+    </MenuComponent>;
+}`,...o.parameters?.docs?.source}}};const $=["Menu","WithoutTitle"];export{n as Menu,o as WithoutTitle,$ as __namedExportsOrder,R as default};
