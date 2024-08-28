7ddc7a3fff5a3e94ccc00c61e630229aa530c31a
t3chguy <t3chguy@users.noreply.github.com>
2024-08-28 10:03:17 +0000

    deploy: 6684cb887725c9a95408a6fa4f099ceeb1481390

---
+++ assets/IconButton.stories-CWmZd29Z.js
@@ -0,0 +1,48 @@
+import{j as r}from"./jsx-runtime-CexXSJP5.js";import{f as g}from"./index-BSM_bQPn.js";import{I as p}from"./IconButton-Y-BQAUFS.js";import{U as m}from"./user-profile-EvkA4CON.js";import"./index-BP8_t0zE.js";import"./index-C8k3Z-3Y.js";import"./index-CSpfAsmC.js";import"./UnstyledButton-C9fNLva5.js";import"./IndicatorIcon-_PMFIuo8.js";import"./Tooltip-2883fbVA.js";import"./floating-ui.react-KXiqSXy2.js";import"./index-Cw6ikIwd.js";const U={title:"Button/IconButton",component:p,tags:["autodocs"],argTypes:{control:{type:"boolean"},onClick:{action:"onClick"}},args:{size:"32px",disabled:!1,children:r.jsx(m,{}),onClick:g()}},I=l=>r.jsxs(r.Fragment,{children:[r.jsx(p,{...l,size:"32px",children:r.jsx(m,{})}),r.jsx(p,{...l,size:"48px",children:r.jsx(m,{})}),r.jsx(p,{...l,size:"64px",children:r.jsx(m,{})})]}),e=I.bind({}),s={args:{}},a={args:{disabled:!0}},o={args:{indicator:"default"}},t={args:{indicator:"default",disabled:!0}},n={args:{indicator:"success"}},c={args:{indicator:"critical"}},i={args:{subtleBackground:!0}},d={args:{label:"label"}},u={args:{destructive:"true"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`args => <>
+    <IconButtonComponent {...args} size="32px">
+      <UserIcon />
+    </IconButtonComponent>
+
+    <IconButtonComponent {...args} size="48px">
+      <UserIcon />
+    </IconButtonComponent>
+
+    <IconButtonComponent {...args} size="64px">
+      <UserIcon />
+    </IconButtonComponent>
+  </>`,...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...a.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    indicator: "default"
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    indicator: "default",
+    disabled: true
+  }
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    indicator: "success"
+  }
+}`,...n.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    indicator: "critical"
+  }
+}`,...c.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    subtleBackground: true
+  }
+}`,...i.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "label"
+  }
+}`,...d.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    destructive: "true"
+  }
+}`,...u.parameters?.docs?.source}}};const v=["Demo","Default","DefaultDisabled","WithIndicator","WithIndicatorDisabled","WithSuccessIndicator","WithCriticalIndicator","WithSubtleBackground","WithLabel","Destructive"];export{s as Default,a as DefaultDisabled,e as Demo,u as Destructive,c as WithCriticalIndicator,o as WithIndicator,t as WithIndicatorDisabled,d as WithLabel,i as WithSubtleBackground,n as WithSuccessIndicator,v as __namedExportsOrder,U as default};
