064e95ddf09dca00e55f08d33dab104dd251d197
RiotRobot <RiotRobot@users.noreply.github.com>
2024-10-01 14:43:10 +0000

    deploy: a5556f98ed8742ecb3f5ceb4718bec761cfd091d

---
+++ assets/MFA.stories-BGS-7MMe.js
@@ -0,0 +1,61 @@
+import{j as p}from"./jsx-runtime-CexXSJP5.js";import{M as t}from"./MFA-1X7uPcX5.js";import"./index-BP8_t0zE.js";import"./index-CSpfAsmC.js";import"./index-Dn9bhuYh.js";import"./index-DD7XHAoh.js";import"./index-Dp4tTk3o.js";import"./index-vl_XNHfj.js";import"./index-CkD-EKAR.js";import"./index-Cw6ikIwd.js";const b={title:"Form/Controls/MFA",component:t,render:({invalid:d,...i})=>p.jsx(t,{...i,"data-invalid":d||void 0}),tags:["autodocs"],parameters:{controls:{include:["defaultValue","disabled","readOnly","invalid"]}},argTypes:{defaultValue:{type:"string"},autoFocus:{type:"boolean"},disabled:{type:"boolean"},readOnly:{type:"boolean"}},args:{defaultValue:"",autoFocus:!1,disabled:!1,readOnly:!1,invalid:!1}},e={parameters:{design:{type:"figma",url:"https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4786"}}},a={args:{defaultValue:"012345"},parameters:{design:{type:"figma",url:"https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4827"}}},n={args:{defaultValue:"012345",disabled:!0},parameters:{design:{type:"figma",url:"https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4821"}}},o={args:{defaultValue:"012345",readOnly:!0},parameters:{design:{type:"figma",url:"https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=2126-21991"}}},r={args:{defaultValue:"012",autoFocus:!0},parameters:{design:{type:"figma",url:"https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4805"}}},s={args:{invalid:!0},parameters:{design:{type:"figma",url:"https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4814"}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4786"
+    }
+  }
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: "012345"
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4827"
+    }
+  }
+}`,...a.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: "012345",
+    disabled: true
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4821"
+    }
+  }
+}`,...n.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: "012345",
+    readOnly: true
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=2126-21991"
+    }
+  }
+}`,...o.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultValue: "012",
+    autoFocus: true
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4805"
+    }
+  }
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=793-4814"
+    }
+  }
+}`,...s.parameters?.docs?.source}}};const v=["Empty","Filled","Disabled","ReadOnly","Focus","Invalid"];export{n as Disabled,e as Empty,a as Filled,r as Focus,s as Invalid,o as ReadOnly,v as __namedExportsOrder,b as default};
