9b9d042dcccae460fb0205c10ce47ff3b5efa1a0
t3chguy <t3chguy@users.noreply.github.com>
2024-10-21 08:07:58 +0000

    deploy: 6ccb4b6049f3bc8e9739d9452c850ed3c7de49f9

---
+++ assets/Alert.stories-C1myCWHI.js
@@ -0,0 +1,39 @@
+import{j as e}from"./jsx-runtime-CexXSJP5.js";import{B as u}from"./Button-C0_Ps78g.js";import{c as b}from"./index-CSpfAsmC.js";import{r as q}from"./index-BP8_t0zE.js";import{C as v,I as C}from"./info-BqHmDWX7.js";import{E as j}from"./error-Dmbc84hO.js";import{C as S}from"./close-Cii-U1fy.js";import{T as p}from"./Text-43GasnQW.js";import{I as A}from"./IconButton-Hy3Mfbae.js";import"./UnstyledButton-C9fNLva5.js";import"./Typography-BWOtt-hZ.js";import"./IndicatorIcon-_PMFIuo8.js";import"./Tooltip-Bttb7TT6.js";import"./floating-ui.react-5MsmdoUF.js";import"./index-B1txWHSv.js";const I="_alert_1bz08_19",N="_content_1bz08_46",w="_title_1bz08_57",z="_icon_1bz08_57",B="_actions_1bz08_73",t={alert:I,content:N,"text-content":"_text-content_1bz08_53",title:w,icon:z,actions:B},l=({type:o,title:g,children:h,className:f,actions:m,onClose:d,...x})=>{const y=b(t.alert,f),_=q.useCallback(c=>{switch(o){case"critical":return e.jsx(j,{...c});case"info":return e.jsx(C,{...c});case"success":return e.jsx(v,{...c})}},[o]);return e.jsxs("div",{...x,className:y,"data-type":o,children:[_({width:24,height:24,className:t.icon,"aria-hidden":!0}),e.jsxs("div",{className:t.content,children:[e.jsxs("div",{className:t["text-content"],children:[e.jsx(p,{size:"md",weight:"semibold",children:g}),e.jsx(p,{size:"sm",weight:"regular",children:h})]}),m&&e.jsx("div",{className:t.actions,children:m})]}),d&&e.jsx(A,{onClick:d,"aria-label":"Close",role:"button",className:t.close,children:e.jsx(S,{})})]})};try{l.displayName="Alert",l.__docgenInfo={description:"An alert component component that is dismissable when passing an `onClose`\nproperty.",displayName:"Alert",props:{type:{defaultValue:null,description:"The type of alert",name:"type",required:!0,type:{name:"enum",value:[{value:'"success"'},{value:'"critical"'},{value:'"info"'}]}},title:{defaultValue:null,description:"The headline of the alert.",name:"title",required:!0,type:{name:"string"}},className:{defaultValue:null,description:"The CSS class name.",name:"className",required:!1,type:{name:"string"}},actions:{defaultValue:null,description:`Actions that will be displayed to the right of the content
+Wraps and stacks actions under content when alert's size is <=600px
+eg
+\`\`\`
+<Alert
+ title='Title'
+ actions={<Button onClick={doSomething}>Yes</Button>}
+/>
+\`\`\``,name:"actions",required:!1,type:{name:"ReactNode"}},onClose:{defaultValue:null,description:`Event callback when dismissing the alert. Determines the display of the
+"close" button at the top right of the alert.
+@param e the event parameters`,name:"onClose",required:!1,type:{name:"((e: MouseEvent<Element, MouseEvent>) => void)"}}}}}catch{}const O={title:"Alert",component:l,tags:["autodocs"],argTypes:{type:{options:["success","critical","info"],control:{type:"inline-radio"}}},args:{title:"Title",children:"Description",onClose:()=>{console.log("Clicked!")}},parameters:{design:{type:"figma",url:"https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=795-7802&mode=design&t=g71gv6nZdJA7QSGB-0"}}},s={args:{type:"success"}},n={args:{type:"critical"}},a={args:{type:"info"}},i={args:{type:"info",title:"Long title. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",children:"Actions are vertically centered against alert content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",actions:e.jsxs(e.Fragment,{children:[e.jsx(u,{children:"Yes"}),e.jsx(u,{children:"No"})]})}},r={...s,args:{...s.args,onClose:void 0}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "success"
+  }
+}`,...s.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "critical"
+  }
+}`,...n.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "info"
+  }
+}`,...a.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "info",
+    title: "Long title. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    children: "Actions are vertically centered against alert content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    actions: <>
+        <Button>Yes</Button>
+        <Button>No</Button>
+      </>
+  }
+}`,...i.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  ...Success,
+  args: {
+    ...Success.args,
+    onClose: undefined
+  }
+}`,...r.parameters?.docs?.source}}};const Z=["Success","Critical","Info","WithActions","WithoutClose"];export{n as Critical,a as Info,s as Success,i as WithActions,r as WithoutClose,Z as __namedExportsOrder,O as default};
---
+++ assets/FloatingMenu.stories-DABqG9x9.js
@@ -0,0 +1,8 @@
+import{j as o}from"./jsx-runtime-CexXSJP5.js";import{U as a}from"./user-profile-EvkA4CON.js";import{N as r,C as s,L as c}from"./notifications-MBLL4uPX.js";import{F as n}from"./FloatingMenu-CL2la2kV.js";import{M as t}from"./MenuItem-B7JmuVZQ.js";import{S as l}from"./Separator-DMvMfjpC.js";import"./index-BP8_t0zE.js";import"./index-CSpfAsmC.js";import"./useId-DIHOOv91.js";import"./Text-43GasnQW.js";import"./Typography-BWOtt-hZ.js";import"./chevron-right-DF47_mtr.js";import"./index-Dp4tTk3o.js";import"./index-DgHR8GBV.js";import"./index-B1txWHSv.js";import"./floating-ui.react-5MsmdoUF.js";const h={title:"Menu/FloatingMenu",component:n,argTypes:{},args:{}},m=i=>o.jsxs(n,{...i,title:"Settings",children:[o.jsx(t,{Icon:a,label:"Profile",onSelect:()=>{}}),o.jsx(t,{Icon:r,label:"Notificationsnotificationsnotifications",onSelect:()=>{}}),o.jsx(t,{Icon:s,label:"Feedback",onSelect:()=>{}}),o.jsx(l,{}),o.jsx(t,{kind:"critical",Icon:c,label:"Sign out",onSelect:()=>{}})]}),e=m.bind({});e.args={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`args => <FloatingMenuComponent {...args} title="Settings">
+    <MenuItem Icon={UserProfileIcon} label="Profile" onSelect={() => {}} />
+    {/* Extra long label to demonstrate word breaking */}
+    <MenuItem Icon={NotificationsIcon} label="Notificationsnotificationsnotifications" onSelect={() => {}} />
+    <MenuItem Icon={ChatProblemIcon} label="Feedback" onSelect={() => {}} />
+    <Separator />
+    <MenuItem kind="critical" Icon={LeaveIcon} label="Sign out" onSelect={() => {}} />
+  </FloatingMenuComponent>`,...e.parameters?.docs?.source}}};const E=["FloatingMenu"];export{e as FloatingMenu,E as __namedExportsOrder,h as default};
