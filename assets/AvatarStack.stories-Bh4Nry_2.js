6dbab352ba28836fb67d7d6614778fbce5ece366
sandhose <sandhose@users.noreply.github.com>
2024-12-13 11:50:08 +0000

    deploy: d73fba6be0f313fe0bd29012cb35445bc15adb1b

---
+++ assets/AvatarStack.stories-Bh4Nry_2.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{c as p}from"./index-lhGYx47h.js";import{r as l}from"./index-DRjF_FHU.js";import{s as r,A as i}from"./Avatar-BPJ7Tfaa.js";const m=`<!--
+Copyright 2023  New Vector Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+<svg>
+  <clipPath id="cpdAvatarClip" clipPathUnits="objectBoundingBox">
+    <!-- 
+        All values below need to be between 0 and 1
+        for objectBoundingBox to work
+     -->
+    <path d="
+        M 0.875 0.16666
+        A 0.5 0.5 0 1 0 0.875 0.83333
+        Q 0.65 0.5 0.875 0.16666
+        Z
+     "/>
+  </clipPath>
+</svg>
+`,o="cpdAvatarClipSvg";let a=0;const s=({children:n,className:c})=>(l.useEffect(()=>{if(a===0){const d=`
+        <div aria-hidden="true" id="${o}" class="${r["clip-path"]}">
+          ${m}
+        </div>
+      `;document.body.insertAdjacentHTML("beforeend",d)}return a++,()=>{a--,a<=0&&document.getElementById(o)?.remove()}},[]),e.jsx("div",{className:p(r["stacked-avatars"],c),children:n}));try{s.displayName="AvatarStack",s.__docgenInfo={description:"Renders a stack of avatars and clips the content appropriately.\n\nThe `type` of avatars should always be set to `round`\nAnd all the avatars should have the same size.",displayName:"AvatarStack",props:{className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const v={title:"Avatar/StackedAvatar",component:s,tags:["autodocs"],argTypes:{},args:{children:e.jsxs(e.Fragment,{children:[e.jsx(i,{name:"Alice",id:"@alice:example.org",size:"200px",src:"/images/__test__/kitten.jpg"}),e.jsx(i,{name:"Bob",id:"@bob:example.org",size:"200px",src:"/images/__test__/kitten2.jpg"})]})}},t={args:{}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...t.parameters?.docs?.source}}};const f=["AvatarStack"];export{t as AvatarStack,f as __namedExportsOrder,v as default};
---
+++ assets/PasswordForm.stories-R0JEjfHD.js
@@ -0,0 +1,51 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{W as p}from"./warning-DNnCy5FR.js";import{P as r}from"./Progress-DssNWzF4.js";import{T as x}from"./Tooltip-DWBiSZRI.js";import{R as s,H as g,S as w}from"./Message-DAbH8XS4.js";import{F as a,L as o}from"./Label-Bz2llXvT.js";import{a as t}from"./Password-BNBFWnWg.js";import"./index-DRjF_FHU.js";import"./index-lhGYx47h.js";import"./index-DlsBsfvk.js";import"./index-Ksfxd92j.js";import"./index-BvEl7W6R.js";import"./index-C8xBnYIG.js";import"./floating-ui.react-C0ln9j1M.js";import"./index-2SmsnS5l.js";import"./index-CGa_gRws.js";import"./check-circle-solid-DvGH8qX5.js";import"./error-DOy7ZFuJ.js";import"./InlineSpinner-DDpdhuC2.js";import"./spinner--mCkS-8r.js";import"./visibility-on-BsZOuBEH.js";import"./Action-lVpIJAKN.js";import"./Text-CZnraUGE.js";const O={title:"Form/Password form",component:s,tags:["autodocs"],decorators:[c=>e.jsx(x,{children:e.jsx("div",{style:{maxWidth:"378px"},children:e.jsx(c,{})})})]},n=c=>{switch(c){case 1:return"Very weak password";case 2:return"Weak password";case 3:return"Strong password";case 4:return"Very strong password";default:return"Password strength"}},d=()=>e.jsx(s,{children:e.jsxs(a,{name:"password",children:[e.jsx(o,{children:"Password"}),e.jsx(t,{}),e.jsx(r,{getValueLabel:n,size:"sm",value:0,max:4})]})}),i=()=>e.jsx(s,{children:e.jsxs(a,{name:"password",children:[e.jsx(o,{children:"Password"}),e.jsx(t,{defaultValue:"password"}),e.jsx(r,{getValueLabel:n,size:"sm",tint:"red",value:1,max:4}),e.jsxs(g,{children:[e.jsx(p,{}),"Guess time: a few seconds. Try adding a few more words."]})]})}),l=()=>e.jsx(s,{children:e.jsxs(a,{name:"password",children:[e.jsx(o,{children:"Password"}),e.jsx(t,{defaultValue:"hunter2"}),e.jsx(r,{getValueLabel:n,size:"sm",tint:"orange",value:2,max:4}),e.jsxs(g,{children:[e.jsx(p,{}),"Guess time: a few days. Try adding a few more words."]})]})}),m=()=>e.jsx(s,{children:e.jsxs(a,{name:"password",children:[e.jsx(o,{children:"Password"}),e.jsx(t,{defaultValue:"this is a strong password"}),e.jsx(r,{getValueLabel:n,size:"sm",tint:"lime",value:3,max:4}),e.jsx(w,{children:"Guess time: years"})]})}),u=()=>e.jsx(s,{children:e.jsxs(a,{name:"password",children:[e.jsx(o,{children:"Password"}),e.jsx(t,{defaultValue:"this is a very strong password"}),e.jsx(r,{getValueLabel:n,size:"sm",tint:"green",value:4,max:4}),e.jsx(w,{children:"Guess time: centruries"})]})});d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`() => {
+  return <Root>
+      <Field name="password">
+        <Label>Password</Label>
+        <PasswordControl />
+        <Progress getValueLabel={getValueLabel} size="sm" value={0} max={4} />
+      </Field>
+    </Root>;
+}`,...d.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`() => {
+  return <Root>
+      <Field name="password">
+        <Label>Password</Label>
+        <PasswordControl defaultValue="password" />
+        <Progress getValueLabel={getValueLabel} size="sm" tint="red" value={1} max={4} />
+        <HelpMessage>
+          <WarningIcon />
+          Guess time: a few seconds. Try adding a few more words.
+        </HelpMessage>
+      </Field>
+    </Root>;
+}`,...i.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`() => {
+  return <Root>
+      <Field name="password">
+        <Label>Password</Label>
+        <PasswordControl defaultValue="hunter2" />
+        <Progress getValueLabel={getValueLabel} size="sm" tint="orange" value={2} max={4} />
+        <HelpMessage>
+          <WarningIcon />
+          Guess time: a few days. Try adding a few more words.
+        </HelpMessage>
+      </Field>
+    </Root>;
+}`,...l.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`() => {
+  return <Root>
+      <Field name="password">
+        <Label>Password</Label>
+        <PasswordControl defaultValue="this is a strong password" />
+        <Progress getValueLabel={getValueLabel} size="sm" tint="lime" value={3} max={4} />
+        <SuccessMessage>Guess time: years</SuccessMessage>
+      </Field>
+    </Root>;
+}`,...m.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`() => {
+  return <Root>
+      <Field name="password">
+        <Label>Password</Label>
+        <PasswordControl defaultValue="this is a very strong password" />
+        <Progress getValueLabel={getValueLabel} size="sm" tint="green" value={4} max={4} />
+        <SuccessMessage>Guess time: centruries</SuccessMessage>
+      </Field>
+    </Root>;
+}`,...u.parameters?.docs?.source}}};const q=["Empty","VeryWeak","Weak","Strong","VeryStrong"];export{d as Empty,m as Strong,u as VeryStrong,i as VeryWeak,l as Weak,q as __namedExportsOrder,O as default};
