98dccbfbf1e83296554d8c1bc8b3953f63aefb7f
Florian Duros <florianduros@element.io>
2025-02-20 17:54:37 +0100

    feat: add ChatFilter component (#317)

---
+++ src/components/ChatFilter/ChatFilter.tsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 New Vector Ltd
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+import React, { ForwardedRef, forwardRef } from "react";
+import {
+  UnstyledButton,
+  UnstyledButtonPropsFor,
+} from "../Button/UnstyledButton";
+import styles from "./ChatFilter.module.css";
+
+type ChatFilterProps = Omit<UnstyledButtonPropsFor<"button">, "disabled"> & {
+  /**
+   * Whether the filter is selected.
+   */
+  selected?: boolean;
+};
+
+/**
+ * A chat filter button.
+ */
+export const ChatFilter = forwardRef(function ChatFilter(
+  { children, selected, ...props }: ChatFilterProps,
+  ref: ForwardedRef<HTMLButtonElement>,
+): React.ReactElement {
+  return (
+    <UnstyledButton
+      {...props}
+      className={styles["chat-filter"]}
+      aria-selected={selected}
+      as="button"
+      ref={ref}
+      tabIndex={0}
+    >
+      {children}
+    </UnstyledButton>
+  );
+});
