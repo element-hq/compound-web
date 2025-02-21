4e92a5f1def86be4ce026a3b26853e2c5daec2a8
florianduros <florianduros@users.noreply.github.com>
2025-02-21 15:01:55 +0000

    deploy: bece139b3c554bd37c82b0130b5c60b16740c9e3

---
+++ components/Menu/MenuTitle.d.ts
@@ -0,0 +1,18 @@
+import { default as React } from '../../../node_modules/react';
+interface MenuTitleProps {
+    /**
+     * The title of the menu.
+     */
+    title: string;
+    /**
+     * The id of the menu title.
+     */
+    id?: string;
+    /**
+     * The CSS class.
+     */
+    className?: string;
+}
+export declare const MenuTitle: React.FC<MenuTitleProps>;
+export {};
+//# sourceMappingURL=MenuTitle.d.ts.map
