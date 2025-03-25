b2bbf51116c2761ea39ad0eb3229c4211468b260
florianduros <florianduros@users.noreply.github.com>
2025-03-25 13:21:35 +0000

    deploy: 12b4558cc65816edbc46e6246fd5822ff4e556f2

---
+++ assets/Link.stories-B7hOzJI8.js
@@ -0,0 +1,7 @@
+import{L as e}from"./Link-Dh-Xmn0F.js";import"./jsx-runtime-C-_spy54.js";import"./index-aRQRyZp7.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-DUolvyrz.js";const c={title:"Link",component:e,tags:["autodocs"],argTypes:{},args:{size:"medium",children:"Text link"}},r={args:{}},s={args:{size:"small"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: "small"
+  }
+}`,...s.parameters?.docs?.source}}};const i=["Round","Small"];export{r as Round,s as Small,i as __namedExportsOrder,c as default};
--- components/ActivityMarker/UnreadCounter.d.ts
+++ components/ActivityMarker/UnreadCounter.d.ts
@@ -1,6 +1,10 @@
 import { HTMLProps } from '../../../node_modules/react';
-interface UnreadCounterProps extends HTMLProps<HTMLSpanElement> {
-    count: string | number;
+interface UnreadCounterProps extends HTMLProps<HTMLDivElement> {
+    /**
+     * The number to display.
+     * If `null`, the counter will be empty.
+     */
+    count: string | number | null;
 }
 export declare function UnreadCounter({ count, ...props }: UnreadCounterProps): import("react/jsx-runtime").JSX.Element;
 export {};
