8f2e05c0ff2bfa009f007bee079e00d21cfaa36d
David Baker <dbkr@users.noreply.github.com>
2025-05-13 16:10:37 +0100

    Add to index

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,7 @@ export { NavBar, NavItem } from "./components/Nav";
 export { Menu } from "./components/Menu/Menu";
 export { MenuItem } from "./components/Menu/MenuItem";
 export { MenuTitle } from "./components/Menu/MenuTitle";
+export { RadioMenuItem } from "./components/Menu/RadioMenuItem";
 export { Progress } from "./components/Progress/Progress";
 export { Search } from "./components/Search/Search";
 export { Separator } from "./components/Separator/Separator";
