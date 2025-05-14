617dcd3e6184c98e5a842a7dede1169c8ad0a797
RiotRobot <RiotRobot@users.noreply.github.com>
2025-05-14 09:09:01 +0000

    deploy: c829503c8c5c157c38e64b2891d6f6e7c44a159c

---
+++ assets/MenuTitle.stories-9i34e9el.js
@@ -0,0 +1,3 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{M as t}from"./MenuTitle-Dks6EK-a.js";import"./Text-CMco4h67.js";import"./Typography-Cv3Gp6a_.js";import"./index-FGJkmGnF.js";import"./_commonjsHelpers-CqkleIqs.js";const l={title:"Menu/MenuTitle",component:t,tags:["autodocs"],argTypes:{},args:{title:"Title"}},s=r=>o.jsx(t,{...r}),e=s.bind({});e.args={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`(args: ComponentProps<typeof MenuTitleComponent>) => {
+  return <MenuTitleComponent {...args} />;
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,l as default};
