7c1adcf7834d5ebcfab4efcb1c39ecaef992b7be
t3chguy <t3chguy@users.noreply.github.com>
2025-05-14 08:31:34 +0000

    deploy: 75f279b181706062e893e0ada84e0e565e0a4b2a

--- assets/Link.stories-Bok2zMxA.js
+++
@@ -1,7 +0,0 @@
-import{L as e}from"./Link-D6urZOif.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-BKyFwriW.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-FGJkmGnF.js";const c={title:"Link",component:e,tags:["autodocs"],argTypes:{},args:{size:"medium",children:"Text link"}},r={args:{}},s={args:{size:"small"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
-  args: {
-    size: "small"
-  }
-}`,...s.parameters?.docs?.source}}};const i=["Round","Small"];export{r as Round,s as Small,i as __namedExportsOrder,c as default};
