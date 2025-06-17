d5e530a53837a91d0ba4f1168cbd5ac540209540
florianduros <florianduros@users.noreply.github.com>
2025-06-17 15:27:52 +0000

    deploy: f127c56d918573a6b13213716236f38899b2f550

---
+++ assets/Button.stories-C-5PeAU_.js
@@ -0,0 +1,67 @@
+import{f as y}from"./index-B9Dy1COi.js";import{B as k}from"./Button-F_dPoep3.js";import{i as l}from"./index-BgmW8dIu.js";import{V as g}from"./visibility-on-D12idZoO.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-FGJkmGnF.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BKyFwriW.js";import"./UnstyledButton-CpKE2ZH4.js";import"./notifications-CK_M8Igr.js";import"./extensions-CWmgbcAh.js";import"./check-circle-solid-Dz1dn1SN.js";import"./check-circle-1U-8D0bv.js";import"./check-Css5R888.js";import"./chevron-down-C_HLOr0c.js";import"./chevron-left-Ijl4h9cp.js";import"./chevron-right-CV8n5cvu.js";import"./close-BMd-NZ_B.js";import"./error-solid-DBDQzJkq.js";import"./info-3LUO8Z6G.js";import"./key-Dv4O-UeM.js";import"./search-CcjgdIpl.js";import"./settings-D-9Z9WE1.js";import"./spinner-CR9mR-g-.js";import"./threads-BUkXAHON.js";import"./user-profile-Cv-igaOS.js";import"./warning-DjySiPv7.js";const K={title:"Button",component:k,tags:["autodocs"],argTypes:{size:{options:["sm","lg"],control:{type:"inline-radio"}},kind:{options:["primary","secondary","tertiary"],control:{type:"inline-radio"}},destructive:{type:"boolean"},disabled:{type:"boolean"},as:{options:["a","button"],control:{type:"inline-radio"}},iconOnly:{type:"boolean"},Icon:{options:Object.keys(l),mapping:l,control:{type:"select"}},onClick:{action:"onClick"}},args:{size:"lg",as:"button",destructive:!1,disabled:!1,children:"Click me!",onClick:y()}},s={args:{kind:void 0,size:void 0}},a={args:{kind:void 0,size:"sm"}},r={args:{kind:"primary"}},o={args:{kind:"primary",destructive:!0}},t={args:{kind:"secondary"}},n={args:{kind:"secondary",destructive:!0}},i={args:{kind:"tertiary"}},c={args:{kind:"tertiary",destructive:!0}},m={args:{...r.args,Icon:g}},d={args:{...r.args,size:"sm",Icon:g}},p={args:{...r.args,disabled:!0}},e={args:{...r.args,as:"a",href:"https://example.org"}},u={args:{...e.args,disabled:!0}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    // test component defaults
+    kind: undefined,
+    size: undefined
+  }
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    // test component defaults
+    kind: undefined,
+    size: "sm"
+  }
+}`,...a.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "primary"
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "primary",
+    destructive: true
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "secondary"
+  }
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "secondary",
+    destructive: true
+  }
+}`,...n.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "tertiary"
+  }
+}`,...i.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "tertiary",
+    destructive: true
+  }
+}`,...c.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    Icon: icons.VisibilityOnIcon
+  }
+}`,...m.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    size: "sm",
+    Icon: icons.VisibilityOnIcon
+  }
+}`,...d.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    disabled: true
+  }
+}`,...p.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Primary.args,
+    as: "a",
+    href: "https://example.org"
+  }
+}`,...e.parameters?.docs?.source}}};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...Link.args,
+    disabled: true
+  }
+}`,...u.parameters?.docs?.source}}};const M=["Default","Small","Primary","PrimaryDestructive","Secondary","SecondaryDestructive","Tertiary","TertiaryDestructive","WithIcon","SmallWithIcon","Disabled","Link","LinkDisabled"];export{s as Default,p as Disabled,e as Link,u as LinkDisabled,r as Primary,o as PrimaryDestructive,t as Secondary,n as SecondaryDestructive,a as Small,d as SmallWithIcon,i as Tertiary,c as TertiaryDestructive,m as WithIcon,M as __namedExportsOrder,K as default};
