c8d418f437368aa13fa8d86d731004dbd47ca5cc
florianduros <florianduros@users.noreply.github.com>
2025-06-20 09:21:54 +0000

    deploy: d523b97e684e354018a4b770e9c6a11901c27ee5

---
+++ assets/SettingsToggle.stories-DB93MTlY.js
@@ -0,0 +1,46 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{r as u}from"./index-BKyFwriW.js";import{L as T}from"./Label-tz7o8eg8.js";import{H as g}from"./Message-D9ShLS-4.js";import{I as y}from"./InlineField-zyxe_UJh.js";import{C as w,a as b}from"./index-B4NPFxk7.js";import{T as C}from"./Toggle-DKEGfZYD.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-FGJkmGnF.js";import"./check-circle-solid-Dz1dn1SN.js";import"./error-solid-DBDQzJkq.js";import"./InlineSpinner-De6ckyvb.js";import"./spinner-CR9mR-g-.js";import"./index-BRw1A0gZ.js";import"./index-C66SyY0f.js";import"./index-CKTwuSvb.js";import"./index-BwBOz17u.js";import"./index-D77-yIvl.js";import"./index-DQw2Bw4b.js";const i=u.forwardRef(function({className:o,label:d,helpMessage:l,disabledMessage:m,name:f,...p},h){return e.jsxs(y,{className:o,name:f,control:e.jsx(C,{ref:h,...p}),children:[e.jsx(T,{children:d}),l&&e.jsx(g,{children:l}),m&&p.disabled&&e.jsx(g,{children:m})]})}),c=u.forwardRef(function(o,d){return e.jsx(w,{asChild:!0,children:e.jsx(i,{ref:d,...o})})});try{i.displayName="SettingsToggleInput",i.__docgenInfo={description:"A toggle component.",displayName:"SettingsToggleInput",props:{label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}},className:{defaultValue:null,description:`The CSS class name for the containing field.
+The CSS class name.`,name:"className",required:!1,type:{name:"string"}},name:{defaultValue:null,description:"The field name.",name:"name",required:!1,type:{name:"string"}},helpMessage:{defaultValue:null,description:"Optional help text to display below the setting.",name:"helpMessage",required:!1,type:{name:"string"}},disabledMessage:{defaultValue:null,description:"Optional help text to display below the setting.",name:"disabledMessage",required:!1,type:{name:"string"}}}}}catch{}try{c.displayName="SettingsToggleControl",c.__docgenInfo={description:"A styled checkbox input wrapped in a `Control` component, for use in Radix forms.",displayName:"SettingsToggleControl",props:{label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}},className:{defaultValue:null,description:`The CSS class name for the containing field.
+The CSS class name.`,name:"className",required:!1,type:{name:"string"}},name:{defaultValue:null,description:"The field name.",name:"name",required:!1,type:{name:"string"}},helpMessage:{defaultValue:null,description:"Optional help text to display below the setting.",name:"helpMessage",required:!1,type:{name:"string"}},disabledMessage:{defaultValue:null,description:"Optional help text to display below the setting.",name:"disabledMessage",required:!1,type:{name:"string"}}}}}catch{}const v={title:"Form/Controls/SettingsToggle",component:i,decorators:[t=>e.jsx(b,{children:e.jsx(t,{})})],tags:["autodocs"],argTypes:{},args:{name:'"any-name"',label:"Setting name",helpMessage:"This is some help text to explain what this setting does."}},a={args:{defaultChecked:!1},parameters:{design:{type:"figma",url:"https://www.figma.com/design/qTWRfItpO3RdCjnTKPu4mL/Settings?node-id=97-15874"}}},s={args:{defaultChecked:!0},parameters:{design:{type:"figma",url:"https://www.figma.com/design/qTWRfItpO3RdCjnTKPu4mL/Settings?node-id=97-15874"}}},n={args:{disabled:!0,disabledMessage:"This feature isn't available to you right now."},parameters:{design:{type:"figma",url:"https://www.figma.com/design/qTWRfItpO3RdCjnTKPu4mL/Settings?node-id=149-38574"}}},r={args:{disabled:!0,defaultChecked:!0,disabledMessage:"This feature cannot be disabled."},parameters:{design:{type:"figma",url:"https://www.figma.com/design/qTWRfItpO3RdCjnTKPu4mL/Settings?node-id=149-38574"}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultChecked: false
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/qTWRfItpO3RdCjnTKPu4mL/Settings?node-id=97-15874"
+    }
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    defaultChecked: true
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/qTWRfItpO3RdCjnTKPu4mL/Settings?node-id=97-15874"
+    }
+  }
+}`,...s.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    disabledMessage: "This feature isn't available to you right now."
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/qTWRfItpO3RdCjnTKPu4mL/Settings?node-id=149-38574"
+    }
+  }
+}`,...n.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    defaultChecked: true,
+    disabledMessage: "This feature cannot be disabled."
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/qTWRfItpO3RdCjnTKPu4mL/Settings?node-id=149-38574"
+    }
+  }
+}`,...r.parameters?.docs?.source}}};const A=["Unchecked","Checked","DisabledUnchecked","DisabledChecked"];export{s as Checked,r as DisabledChecked,n as DisabledUnchecked,a as Unchecked,A as __namedExportsOrder,v as default};
