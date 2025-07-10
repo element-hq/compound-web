82b5e85d716034a41d9a69fa24632efcab116f13
RiotRobot <RiotRobot@users.noreply.github.com>
2025-07-10 08:04:47 +0000

    deploy: 58b996c54501e5dd1026b5e90509a7551d52b115

---
+++ assets/Alert.stories-BzeSLJQ0.js
@@ -0,0 +1,39 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{B as u}from"./Button-F_dPoep3.js";import{c as q}from"./index-FGJkmGnF.js";import{r as v}from"./index-BKyFwriW.js";import{C}from"./check-circle-1U-8D0bv.js";import{E as b}from"./error-solid-DBDQzJkq.js";import{I as j}from"./info-3LUO8Z6G.js";import{C as N}from"./close-BMd-NZ_B.js";import{T as p}from"./Text-CMco4h67.js";import{I as S}from"./IconButton-BiQIzZW4.js";import"./UnstyledButton-CpKE2ZH4.js";import"./_commonjsHelpers-CqkleIqs.js";import"./Typography-Cv3Gp6a_.js";import"./IndicatorIcon-f90a8Ogl.js";import"./Tooltip-CNzGrA1g.js";import"./floating-ui.react-CUHBVVHR.js";import"./floating-ui.react-dom-6ZVBEX4r.js";import"./index-D77-yIvl.js";import"./index-DQw2Bw4b.js";const I="_alert_zx76t_10",A="_content_zx76t_37",w="_icon_zx76t_48",B="_actions_zx76t_64",t={alert:I,content:A,"text-content":"_text-content_zx76t_44",icon:w,actions:B},l=({type:s,title:g,children:h,className:f,actions:m,onClose:d,...x})=>{const y=q(t.alert,f),_=v.useCallback(c=>{switch(s){case"critical":return e.jsx(b,{...c});case"info":return e.jsx(j,{...c});case"success":return e.jsx(C,{...c})}},[s]);return e.jsxs("div",{...x,className:y,"data-type":s,children:[_({width:24,height:24,className:t.icon,"aria-hidden":!0}),e.jsxs("div",{className:t.content,children:[e.jsxs("div",{className:t["text-content"],children:[e.jsx(p,{size:"md",weight:"semibold",children:g}),e.jsx(p,{size:"sm",weight:"regular",children:h})]}),m&&e.jsx("div",{className:t.actions,children:m})]}),d&&e.jsx(S,{onClick:d,"aria-label":"Close",role:"button",className:t.close,children:e.jsx(N,{})})]})};try{l.displayName="Alert",l.__docgenInfo={description:"An alert component component that is dismissable when passing an `onClose`\nproperty.",displayName:"Alert",props:{type:{defaultValue:null,description:"The type of alert",name:"type",required:!0,type:{name:"enum",value:[{value:'"success"'},{value:'"critical"'},{value:'"info"'}]}},title:{defaultValue:null,description:"The headline of the alert.",name:"title",required:!0,type:{name:"string"}},className:{defaultValue:null,description:"The CSS class name.",name:"className",required:!1,type:{name:"string"}},actions:{defaultValue:null,description:`Actions that will be displayed to the right of the content
+Wraps and stacks actions under content when alert's size is <=600px
+eg
+\`\`\`
+<Alert
+ title='Title'
+ actions={<Button onClick={doSomething}>Yes</Button>}
+/>
+\`\`\``,name:"actions",required:!1,type:{name:"ReactNode"}},onClose:{defaultValue:null,description:`Event callback when dismissing the alert. Determines the display of the
+"close" button at the top right of the alert.
+@param e the event parameters`,name:"onClose",required:!1,type:{name:"((e: MouseEvent<Element, MouseEvent>) => void)"}}}}}catch{}const K={title:"Alert",component:l,tags:["autodocs"],argTypes:{type:{options:["success","critical","info"],control:{type:"inline-radio"}}},args:{title:"Title",children:"Description",onClose:()=>{console.log("Clicked!")}},parameters:{design:{type:"figma",url:"https://www.figma.com/file/rTaQE2nIUSLav4Tg3nozq7/Compound-Web-Components?type=design&node-id=795-7802&mode=design&t=g71gv6nZdJA7QSGB-0"}}},o={args:{type:"success"}},n={args:{type:"critical"}},a={args:{type:"info"}},i={args:{type:"info",title:"Long title. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",children:"Actions are vertically centered against alert content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",actions:e.jsxs(e.Fragment,{children:[e.jsx(u,{children:"Yes"}),e.jsx(u,{children:"No"})]})}},r={...o,args:{...o.args,onClose:void 0}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "success"
+  }
+}`,...o.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "critical"
+  }
+}`,...n.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "info"
+  }
+}`,...a.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    type: "info",
+    title: "Long title. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    children: "Actions are vertically centered against alert content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    actions: <>
+        <Button>Yes</Button>
+        <Button>No</Button>
+      </>
+  }
+}`,...i.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  ...Success,
+  args: {
+    ...Success.args,
+    onClose: undefined
+  }
+}`,...r.parameters?.docs?.source}}};const P=["Success","Critical","Info","WithActions","WithoutClose"];export{n as Critical,a as Info,o as Success,i as WithActions,r as WithoutClose,P as __namedExportsOrder,K as default};
---
+++ assets/Separator.stories-UZsAN27u.js
@@ -0,0 +1,13 @@
+import{S as e}from"./Separator-Dcxu-U7C.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-FGJkmGnF.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-BKyFwriW.js";import"./index-BwBOz17u.js";import"./index-D77-yIvl.js";import"./index-DQw2Bw4b.js";import"./index-C66SyY0f.js";import"./floating-ui.react-CUHBVVHR.js";import"./floating-ui.react-dom-6ZVBEX4r.js";const y={title:"Separator",component:e,tags:["autodocs"],argTypes:{},args:{}},r={args:{kind:"primary"}},a={args:{kind:"secondary"}},o={args:{kind:"section"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "primary"
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "secondary"
+  }
+}`,...a.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    kind: "section"
+  }
+}`,...o.parameters?.docs?.source}}};const k=["Primary","Secondary","Section"];export{r as Primary,a as Secondary,o as Section,k as __namedExportsOrder,y as default};
