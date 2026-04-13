c22cdeceb62b89350f5c888589e0518f8a1031b2
florianduros <florianduros@users.noreply.github.com>
2026-04-13 13:36:53 +0000

    deploy: a5578a22e73dfd98aa647533f6695e1de275d0bd

--- assets/Icon.stories-BOj3OM_Y.js
+++
@@ -1,3 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./dist-BzcXHAJr.js";import{n,t as r}from"./threads-DRBH6MDF.js";var i,a,o;e((()=>{t(),r(),i={title:`Icon`,component:n,argTypes:{}},a={args:{}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...a.parameters?.docs?.source}}},o=[`Icon`]}))();export{a as Icon,o as __namedExportsOrder,i as default};
---
+++ assets/Menu.stories-CU5xpfxn.js
@@ -0,0 +1,10 @@
+import{a as e,n as t}from"./chunk-BneVvdWh.js";import{J as n}from"./iframe-DfpZbwcW.js";import{t as r}from"./jsx-runtime-DweqXWlm.js";import{t as i}from"./dist-CKclY2G_.js";import{n as a,t as o}from"./Button-DZIAlVVq.js";import{a as s,i as c,n as l,o as u,r as d,t as f}from"./notifications-BG_oWI7j.js";import{n as p,t as m}from"./user-profile-DtezBJ4B.js";import{n as h,t as g}from"./MenuTitle-0fsoLQbi.js";import{n as _,t as v}from"./MenuItem-DpXYlXyx.js";import{n as y,t as b}from"./Menu-D2ixfPL_.js";import{n as x,t as S}from"./Separator-DmLoigTQ.js";var C,w,T,E,D,O,k;t((()=>{C=e(n(),1),i(),m(),f(),u(),d(),y(),_(),x(),a(),h(),w=r(),T=e=>{let[t,n]=(0,C.useState)(!0);return(0,w.jsxs)(b,{...e,open:t,onOpenChange:n,trigger:(0,w.jsx)(o,{children:`Open menu`}),align:`start`,children:[(0,w.jsx)(v,{Icon:p,label:`Profile`,onSelect:()=>{}}),(0,w.jsx)(v,{Icon:l,label:`Notifications`,onSelect:()=>{}}),(0,w.jsx)(g,{title:`Other section`}),(0,w.jsx)(v,{Icon:l,label:`Other Notifications`,onSelect:()=>{}}),(0,w.jsx)(v,{Icon:s,label:`Feedback`,onSelect:()=>{}}),(0,w.jsx)(S,{}),(0,w.jsx)(v,{kind:`critical`,Icon:c,label:`Sign out`,onSelect:()=>{}})]})},E={title:`Menu`,component:T,tags:[`autodocs`,`axe-exclude`],argTypes:{},args:{}},D={args:{title:`Today's Menu`}},O={args:{title:`Untitled Menu`,showTitle:!1}},D.parameters={...D.parameters,docs:{...D.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Today's Menu"
+  }
+}`,...D.parameters?.docs?.source}}},O.parameters={...O.parameters,docs:{...O.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Untitled Menu",
+    showTitle: false
+  }
+}`,...O.parameters?.docs?.source}}},k=[`Menu`,`WithoutTitle`]}))();export{D as Menu,O as WithoutTitle,k as __namedExportsOrder,E as default};
