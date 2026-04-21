b7436fccece055f13ab9d983504c439efb49e104
Half-Shot <will@half-shot.uk>
2026-04-21 13:43:07 +0100

    Revert unused changes

--- src/components/Button/IconButton/IconButton.tsx
+++ src/components/Button/IconButton/IconButton.tsx
@@ -30,11 +30,6 @@ type IconButtonProps = UnstyledButtonPropsFor<"button"> & {
    */
   size?: CSSStyleDeclaration["height"];
   /**
-   * The amount of margin to give the icon itself.
-   * @default 0 / unset.
-   */
-  margin?: CSSStyleDeclaration["margin"];
-  /**
    * The icon button indicator dot displayed on the top right
    * As in IndicatorIcon
    */
@@ -76,7 +71,6 @@ export const IconButton = forwardRef<
     className,
     indicator,
     size = "32px",
-    margin = "0px",
     style,
     disabled,
     destructive,
@@ -100,7 +94,6 @@ export const IconButton = forwardRef<
       style={
         {
           "--cpd-icon-button-size": size,
-          "--cpd-icon-button-margin": margin,
           ...style,
         } as React.CSSProperties
       }
@@ -111,7 +104,6 @@ export const IconButton = forwardRef<
     >
       <IndicatorIcon
         indicator={indicator}
-        size="auto"
         colour={disabled ? "var(--cpd-color-icon-disabled)" : undefined}
       >
         {React.Children.only(children)}
