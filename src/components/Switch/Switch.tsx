32926c7bc12ca90aa5cc9a948dcadb5cd8a5c88c
Half-Shot <will@half-shot.uk>
2026-04-24 09:53:02 +0100

    Fix copyrights

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2026 Element Creations Ltd
+Copyright 2026 Element Creations Ltd.
 Copyright 2023, 2024 New Vector Ltd.
 
 SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
--- src/components/Switch/index.ts
+++ src/components/Switch/index.ts
@@ -1,1 +1,7 @@
+/*
+Copyright 2026 Element Creations Ltd.
+
+SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+Please see LICENSE in the repository root for full details.
+*/
 export * from "./Switch";
